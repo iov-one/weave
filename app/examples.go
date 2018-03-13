c911caebb3cdd688ba485da77dad75d381f4bcab
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-03-13 20:19:31 +0100

    Basic bov app compiles

---
+++ app/examples.go
@@ -0,0 +1,77 @@
+package app
+
+import (
+	"github.com/confio/weave/commands"
+	"github.com/confio/weave/crypto"
+	"github.com/confio/weave/x"
+	"github.com/confio/weave/x/cash"
+	"github.com/confio/weave/x/sigs"
+
+	"github.com/iov-one/bov-core/x/namecoin"
+)
+
+// Examples generates some example structs to dump out with testgen
+func Examples() []commands.Example {
+	wallet := &namecoin.Wallet{
+		Name: "example",
+		Coins: []*x.Coin{
+			&x.Coin{Whole: 50000, Ticker: "ETH"},
+			&x.Coin{Whole: 150, Fractional: 567000, Ticker: "BTC"},
+		},
+	}
+
+	token := &namecoin.Token{
+		Name:    "My special coin",
+		SigFigs: 8,
+	}
+
+	priv := crypto.GenPrivKeyEd25519()
+	pub := priv.PublicKey()
+	user := &sigs.UserData{
+		PubKey:   pub,
+		Sequence: 17,
+	}
+
+	dst := crypto.GenPrivKeyEd25519().PublicKey().Address()
+	amt := x.NewCoin(250, 0, "ETH")
+	msg := &cash.SendMsg{
+		Amount: &amt,
+		Dest:   dst,
+		Src:    pub.Address(),
+		Memo:   "Test payment",
+	}
+
+	nameMsg := &namecoin.SetWalletNameMsg{
+		Address: pub.Address(),
+		Name:    "myname",
+	}
+
+	tokenMsg := &namecoin.NewTokenMsg{
+		Ticker:  "ATM",
+		Name:    "At the moment",
+		SigFigs: 3,
+	}
+
+	unsigned := Tx{
+		Sum: &Tx_SendMsg{msg},
+	}
+	tx := unsigned
+	sig, err := sigs.SignTx(priv, &tx, "test-123", 17)
+	if err != nil {
+		panic(err)
+	}
+	tx.Signatures = []*sigs.StdSignature{sig}
+
+	return []commands.Example{
+		{"wallet", wallet},
+		{"token", token},
+		{"priv_key", priv},
+		{"pub_key", pub},
+		{"user", user},
+		{"send_msg", msg},
+		{"name_msg", nameMsg},
+		{"token_msg", tokenMsg},
+		{"unsigned_tx", &unsigned},
+		{"signed_tx", &tx},
+	}
+}
