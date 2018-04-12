ac8c1eb1e56809aac56250fb7232877d542832e2
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-04-12 16:15:36 +0200

    Get x/sigs to compile with Permission not Address

--- x/sigs/context.go
+++ x/sigs/context.go
@@ -14,11 +14,12 @@ type contextKey int // local to the auth module
 
 const (
 	contextKeySigners contextKey = iota
+	extensionName                = "sigs"
 )
 
 // withSigners is a private method, as only this module
 // can add a signer
-func withSigners(ctx weave.Context, signers []weave.Address) weave.Context {
+func withSigners(ctx weave.Context, signers []weave.Permission) weave.Context {
 	return context.WithValue(ctx, contextKeySigners, signers)
 }
 
@@ -30,20 +31,20 @@ var _ x.Authenticator = Authenticate{}
 
 // GetPermissions returns who signed the current Context.
 // May be empty
-func (a Authenticate) GetPermissions(ctx weave.Context) []weave.Address {
+func (a Authenticate) GetPermissions(ctx weave.Context) []weave.Permission {
 	// (val, ok) form to return nil instead of panic if unset
-	val, _ := ctx.Value(contextKeySigners).([]weave.Address)
+	val, _ := ctx.Value(contextKeySigners).([]weave.Permission)
 	// if we were paranoid about our own code, we would deep-copy
 	// the signers here
 	return val
 }
 
-// HasPermission returns true if the given address
+// HasAddress returns true if the given address
 // had signed in the current Context.
-func (a Authenticate) HasPermission(ctx weave.Context, addr weave.Address) bool {
+func (a Authenticate) HasAddress(ctx weave.Context, addr weave.Address) bool {
 	signers := a.GetPermissions(ctx)
 	for _, s := range signers {
-		if addr.Equals(s) {
+		if addr.Equals(s.Hash()) {
 			return true
 		}
 	}
