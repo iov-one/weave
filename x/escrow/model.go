9f96d6d98c512fbea9c55e8cb6d27b6a9b47cf19
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-04-13 14:48:20 +0200

    Add basic db model

---
+++ x/escrow/model.go
@@ -0,0 +1,124 @@
+package escrow
+
+import (
+	"errors"
+
+	"github.com/confio/weave"
+	"github.com/confio/weave/orm"
+)
+
+const (
+	// BucketName is where we store the escrows
+	BucketName = "esc"
+	// SequenceName is an auto-increment ID counter for escrows
+	SequenceName = "id"
+)
+
+var _ orm.CloneableData = (*Escrow)(nil)
+
+// Validate ensures the escrow is valid
+func (e *Escrow) Validate() error {
+	if e.Sender == nil {
+		return errors.New("TODO: missing sender")
+	}
+	// Copied from CreateEscrowMsg.Validate
+	// TODO: code reuse???
+	if e.Arbiter == nil {
+		return errors.New("TODO: missing arbiter")
+	}
+	if e.Recipient == nil {
+		return errors.New("TODO: missing recipient")
+	}
+	if e.Timeout <= 0 {
+		return errors.New("TODO: invalid timeout")
+	}
+	if len(e.Memo) > maxMemoSize {
+		return errors.New("TODO: invalid memo")
+		// return ErrInvalidMemo("Memo too long")
+	}
+	if err := validateAmount(e.Amount); err != nil {
+		return err
+	}
+	return validatePermissions(e.Arbiter, e.Sender, e.Recipient)
+}
+
+// Copy makes a new set with the same coins
+func (e *Escrow) Copy() orm.CloneableData {
+	return &Escrow{
+		Sender:    e.Sender,
+		Arbiter:   e.Arbiter,
+		Recipient: e.Recipient,
+		Amount:    e.Amount,
+		Timeout:   e.Timeout,
+		Memo:      e.Memo,
+	}
+}
+
+// AsEscrow safely extracts a Escrow value from the object
+func AsEscrow(obj orm.Object) *Escrow {
+	if obj == nil || obj.Value() == nil {
+		return nil
+	}
+	return obj.Value().(*Escrow)
+}
+
+// NewEscrow generates a new Escrow object
+// TODO: auto-generate sequence
+// func NewEscrow(ticker, name string, sigFigs int32) orm.Object {
+// 	value := &Escrow{
+// 		Name:    name,
+// 		SigFigs: sigFigs,
+// 	}
+// 	return orm.NewSimpleObj([]byte(ticker), value)
+// }
+
+//--- Bucket - handles escrows
+
+// Bucket is a type-safe wrapper around orm.Bucket
+type Bucket struct {
+	orm.Bucket
+}
+
+// NewBucket initializes a Bucket with default name
+//
+// inherit Get and Save from orm.Bucket
+// add Create
+func NewBucket() Bucket {
+	return Bucket{
+		Bucket: orm.NewBucket(BucketName,
+			orm.NewSimpleObj(nil, new(Escrow))),
+	}
+}
+
+// TODO: remove??? On afterthought, this is probably never needed
+// // GetOrCreate will return the token if found, or create one
+// // with the given name otherwise.
+// func (b Bucket) GetOrCreate(db weave.KVStore, ticker string) (orm.Object, error) {
+//  obj, err := b.Get(db, ticker)
+//  if err == nil && obj == nil {
+//      obj = NewToken(ticker, "", DefaultSigFigs)
+//  }
+//  return obj, err
+// }
+
+// Create will calculate the next sequence number and then
+// store the escrow there.
+// Saves the object and returns it (to inspect the ID)
+func (b Bucket) Create(db weave.KVStore, escrow *Escrow) (orm.Object, error) {
+	seq := b.Bucket.Sequence(SequenceName)
+	key := seq.NextVal(db)
+	obj := orm.NewSimpleObj(key, escrow)
+	err := b.Bucket.Save(db, obj)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+// Save enforces the proper type
+func (b Bucket) Save(db weave.KVStore, obj orm.Object) error {
+	if _, ok := obj.Value().(*Escrow); !ok {
+		return orm.ErrInvalidObject(obj.Value())
+	}
+	return b.Bucket.Save(db, obj)
+}
