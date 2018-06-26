9599cf4e3904eb9c18b79ee1a20ff173370ecb75
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-06-26 19:44:49 +0200

    Start tendermint with initialize bcp-demo app

--- client_test.go
+++ client_test.go
@@ -2,14 +2,38 @@ package utils
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"github.com/tendermint/tendermint/rpc/client"
 	rpctest "github.com/tendermint/tendermint/rpc/test"
 )
+
+// blocks go by fast, no need to wait seconds....
+func fastWaiter(delta int64) (abort error) {
+	delay := time.Duration(delta) * 5 * time.Millisecond
+	time.Sleep(delay)
+	return nil
+}
+
+var _ client.Waiter = fastWaiter
 
 func TestMainSetup(t *testing.T) {
 	config := rpctest.GetConfig()
 	assert.Equal(t, "SetInTestMain", config.Moniker)
+
+	conn := client.NewLocal(node)
+	status, err := conn.Status()
+	require.NoError(t, err)
+	assert.Equal(t, "SetInTestMain", status.NodeInfo.Moniker)
+
+	// wait for some blocks to be produced....
+	client.WaitForHeight(conn, 5, fastWaiter)
+	status, err = conn.Status()
+	require.NoError(t, err)
+	assert.True(t, status.SyncInfo.LatestBlockHeight > 4)
+
 	// assert.Equal(t, "/dev/null", config.GenesisFile())
 }
--- main_test.go
+++ main_test.go
@@ -1,25 +1,51 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"testing"
 
-	"github.com/tendermint/abci/example/kvstore"
+	"github.com/confio/weave"
+	"github.com/confio/weave/x"
+	"github.com/iov-one/bcp-demo/app"
+
+	abci "github.com/tendermint/abci/types"
+	cfg "github.com/tendermint/tendermint/config"
 	nm "github.com/tendermint/tendermint/node"
 	rpctest "github.com/tendermint/tendermint/rpc/test"
+	tm "github.com/tendermint/tendermint/types"
+	"github.com/tendermint/tmlibs/log"
 )
 
+// configuration for genesis
+var initBalance = x.Coin{
+	Whole:  100200300,
+	Ticker: "TOOL",
+}
+
+// adjust this to get debug output
+var logger = log.NewNopLogger() // log.NewTMLogger()
+
+// useful values for test cases
 var node *nm.Node
+var faucet *PrivateKey
 
 func TestMain(m *testing.M) {
-	// TODO: use our app
-	// start a tendermint node (and kvstore) in the background to test against
-	app := kvstore.NewKVStoreApplication()
+	faucet = GenPrivateKey()
 
 	// TODO: check out config file...
 	config := rpctest.GetConfig()
 	config.Moniker = "SetInTestMain"
 
+	// set up our application
+	admin := faucet.PublicKey().Address()
+	app, err := initApp(config, admin)
+	if err != nil {
+		panic(err) // what else to do???
+	}
+
+	// run the app inside a tendermint instance
 	node = rpctest.StartTendermint(app)
 	code := m.Run()
 
@@ -27,4 +53,43 @@ func TestMain(m *testing.M) {
 	node.Stop()
 	node.Wait()
 	os.Exit(code)
+}
+
+func initApp(config *cfg.Config, addr weave.Address) (abci.Application, error) {
+	bcp, err := app.GenerateApp(config.RootDir, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	// generate genesis file...
+	err = initGenesis(config.GenesisFile(), addr)
+	return bcp, err
+}
+
+func initGenesis(filename string, addr weave.Address) error {
+	// load genesis
+	doc, err := tm.GenesisDocFromFile(filename)
+	if err != nil {
+		return err
+	}
+
+	// set app state
+	token, _ := json.Marshal(initBalance)
+	appState := fmt.Sprintf(`{
+        "wallets": [{
+            "name": "faucet",
+            "address": "%s",
+            "coins": [%s]
+        }],
+        "tokens": [{
+            "ticker": "%s",
+            "name": "Default token",
+            "sig_figs": 9
+        }]
+    }`, addr, string(token), initBalance.Ticker)
+	fmt.Println(appState)
+	doc.AppStateJSON = []byte(appState)
+
+	// save file
+	return doc.SaveAs(filename)
 }
