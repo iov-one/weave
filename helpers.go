657437c3897e9eb330bac42159441f15c3973dd6
lehajam <Lehajam@gmail.com>
2018-06-29 14:45:17 +0200

    Adding comment to ToString

--- helpers.go
+++ helpers.go
@@ -2,6 +2,8 @@ package utils
 
 import "encoding/json"
 
+// ToString is a generic stringer which outputs
+// a struct in its equivalent (indented) json representation
 func ToString(d interface{}) string {
 	s, err := json.MarshalIndent(d, "", "	")
 	if err != nil {
