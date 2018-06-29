c33b8d5e83efec3d3b160ecbf93c40372776a6b5
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-06-29 18:27:32 +0200

    Add comments

--- conn.go
+++ conn.go
@@ -18,10 +18,14 @@ as we need to make changes
 
 // NewLocalConnection wraps an in-process node with a client, useful for tests
 func NewLocalConnection(node *nm.Node) client.Client {
+	// This uses the standard tendermint implementation
 	return client.NewLocal(node)
 }
 
 // NewHTTPConnection takes a URL and sends all requests to the remote node
 func NewHTTPConnection(remote string) client.Client {
+	// This uses a custom implementation with support for https/wss
+	// We can make local changes easily in tools, and add them back
+	// upstream as we just copied some classes over.
 	return rpcclient.NewHTTP(remote, "/websocket")
 }
