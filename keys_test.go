446e39bbcb9099305061194a7dd760cba86a70f6
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-03 16:37:21 +0200

    Revert "Add failing test"
    This reverts commit ce0bd3524d306731d739094cccb90447ddf46360.

--- keys_test.go
+++ keys_test.go
@@ -169,7 +169,3 @@ func TestDecodesCliKey(t *testing.T) {
 	require.NoError(t, err)
 	assert.EqualValues(t, address, key.PublicKey().Address())
 }
-
-func TestShouldFail(t *testing.T) {
-	t.Fail()
-}
