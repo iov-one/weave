8554a09db5caa02fbf313356dcae5035f7d66247
Alex Peters <alpe@users.noreply.github.com>
2018-08-27 16:36:09 +0200

    Init mycoind on run.sh

--- examples/mycoind/run.sh
+++ examples/mycoind/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Assumes you have everything set up
-# and have initialised the chain
-
 docker rm -f iov-tm || true
 docker run --name iov-tm --rm -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 init
 docker run --name iov-tm -d -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 node --proxy_app="tcp://host.docker.internal:46658"
 
+mycoind init
 mycoind start
