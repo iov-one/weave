5c2092bb3ec0f0caef169ff189611aadb61f4b79
Alex Peters <alpe@users.noreply.github.com>
2018-08-27 16:29:10 +0200

    Replace tendermint Gopath update

--- examples/mycoind/run.sh
+++ examples/mycoind/run.sh
@@ -3,5 +3,8 @@
 # Assumes you have everything set up
 # and have initialised the chain
 
-tendermint node --home ~/.mycoind --p2p.skip_upnp > ~/.mycoind/tendermint.log &
+docker rm -f iov-tm || true
+docker run --name iov-tm --rm -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 init
+docker run --name iov-tm -d -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 node --proxy_app="tcp://host.docker.internal:46658"
+
 mycoind start
