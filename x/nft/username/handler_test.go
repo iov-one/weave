dc85fd9d1cb305e8f205cf17f4d32eae0a8ef48b
Alex Peters <alpe@users.noreply.github.com>
2018-09-20 10:20:22 +0200

    Add query test

--- x/nft/username/handler_test.go
+++ x/nft/username/handler_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/iov-one/weave"
 	"github.com/iov-one/weave/store"
 	"github.com/iov-one/weave/x"
 	"github.com/iov-one/weave/x/nft"
@@ -22,9 +23,6 @@ func TestHandleIssueTokenMsg(t *testing.T) {
 	bucket := username.NewBucket()
 	o, _ := bucket.Create(db, bob.Address(), []byte("existing@example.com"), []username.PublicKey{myPubKey})
 	bucket.Save(db, o)
-	o, err := bucket.Get(db, []byte("existing@example.com"))
-	require.NoError(t, err)
-	require.NotNil(t, o)
 
 	handler := username.NewIssueHandler(helpers.Authenticate(alice), nil, bucket)
 
@@ -107,17 +105,48 @@ func TestHandleIssueTokenMsg(t *testing.T) {
 				assert.Error(t, err)
 				return
 			}
-			assert.NoError(t, err)
-			assert.NotNil(t, res)
+			require.NoError(t, err)
+			require.NotNil(t, res)
 			assert.Equal(t, uint32(0), res.ToABCI().Code)
 
 			// and persisted
 			o, err := bucket.Get(db, spec.id)
-			assert.NoError(t, err)
-			assert.NotNil(t, o)
+			require.NoError(t, err)
+			require.NotNil(t, o)
 			u, _ := username.AsUsername(o)
 			assert.Equal(t, spec.details.Keys, u.GetPubKeys())
 			// todo: verify approvals
 		})
 	}
+}
+
+func TestQueryTokenByName(t *testing.T) {
+	var helpers x.TestHelpers
+	_, alice := helpers.MakeKey()
+	_, bob := helpers.MakeKey()
+	myPubKeys := []username.PublicKey{{Data: []byte("my Public key"), Algorithm: "myAlgorithm"}}
+
+	db := store.MemStore()
+	bucket := username.NewBucket()
+	o1, _ := bucket.Create(db, alice.Address(), []byte("alice@example.com"), myPubKeys)
+	bucket.Save(db, o1)
+	o2, _ := bucket.Create(db, bob.Address(), []byte("bob@example.com"), myPubKeys)
+	bucket.Save(db, o2)
+
+	qr := weave.NewQueryRouter()
+	username.RegisterQuery(qr)
+	// when
+	h := qr.Handler("/nft/usernames")
+	require.NotNil(t, h)
+	mods, err := h.Query(db, "", []byte("alice@example.com"))
+	// then
+	require.NoError(t, err)
+	require.Len(t, mods, 1)
+
+	assert.Equal(t, bucket.DBKey([]byte("alice@example.com")), mods[0].Key)
+	got, err := bucket.Parse(nil, mods[0].Value)
+	require.NoError(t, err)
+	x, err := username.AsUsername(got)
+	require.NoError(t, err)
+	assert.Equal(t, myPubKeys, x.GetPubKeys())
 }
