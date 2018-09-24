3a38f678ccb59165532e1d207a51c9d9f97a48a9
Alexander Peters <alpe@users.noreply.github.com>
2018-09-24 14:30:37 +0200

    Add blockchain skeleton (#135)

---
+++ x/nft/blockchain/bucket.go
@@ -0,0 +1,46 @@
+package blockchain
+
+import (
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/orm"
+	"github.com/iov-one/weave/x/nft"
+)
+
+const (
+	BucketName = "bchnft"
+)
+
+type Bucket struct {
+	orm.Bucket
+}
+
+func NewBucket() Bucket {
+	return Bucket{
+		Bucket: nft.WithOwnerIndex(orm.NewBucket(BucketName, NewBlockchainToken(nil, nil))),
+	}
+}
+
+func NewBlockchainToken(key []byte, owner weave.Address) *orm.SimpleObj {
+	return orm.NewSimpleObj(key, &BlockchainToken{
+		Base:    nft.NewNonFungibleToken(key, owner),
+		Details: &TokenDetails{},
+	})
+}
+
+func (b Bucket) Create(db weave.KVStore, owner weave.Address, id []byte, networks []Network) (orm.Object, error) {
+	obj, err := b.Get(db, id)
+	switch {
+	case err != nil:
+		return nil, err
+	case obj != nil:
+		return nil, orm.ErrUniqueConstraint("id exists already")
+	}
+	obj = NewBlockchainToken(id, owner)
+	humanAddress, err := AsBlockchain(obj)
+	if err != nil {
+		return nil, err
+	}
+	_ = humanAddress // todo set payload proper
+	//return obj, humanAddress.SetNetworks(owner, networks)
+	return obj, nil
+}
