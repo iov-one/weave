fda722aa21f0d3b7e47e77f50d2a02486a1ef3d6
Roman Useinov <roman.useinov@gmail.com>
2018-09-24 12:15:49 +0200

    more validation and todo

--- x/nft/msg.go
+++ x/nft/msg.go
@@ -23,16 +23,18 @@ func (*RemoveApprovalMsg) Path() string {
 	return pathRemoveApproval
 }
 
+//TODO: Fields are exactly the same, except Add has ApprovalOptions. Shall we unify common
+// fields to baseApprovalMessage?
 func (m *AddApprovalMsg) Validate() error {
+	var validation *Validation
 	if err := weave.Address(m.Address).Validate(); err != nil {
 		return err
 	}
-	if m.Action == "" {
-		return errors.ErrInternal("action must not be empty")
+	if !validation.IsValidAction(m.Action) {
+		return errors.ErrInternal("action must be valid")
 	}
-	//TODO: Figure out whether we need to incorporate same check as in NFT
-	if len(m.Id) == 0 {
-		return errors.ErrInternal("id must not be empty")
+	if !validation.IsValidTokenID(m.Id) {
+		return errors.ErrInternal("id must be valid")
 	}
 	return nil
 }
