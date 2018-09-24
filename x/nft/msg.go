35035e4161d4768f332575b11f4c70ebdf788d3c
Roman Useinov <roman.useinov@gmail.com>
2018-09-24 15:20:42 +0200

    Frature/nft add remove approvals (#127)
    * add approval messages
    
    * initial msg
    
    * some more todos and validation
    
    * ApprovalOptions not nullable, nor they should be
    
    * fix up approvals
    
    * fix hanlder paths
    
    * incorporate helpers validation
    
    * more validation and todo

---
+++ x/nft/msg.go
@@ -0,0 +1,54 @@
+package nft
+
+import (
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
+)
+
+var _ weave.Msg = (*AddApprovalMsg)(nil)
+var _ weave.Msg = (*RemoveApprovalMsg)(nil)
+
+const (
+	pathAddApproval    = "nft/approval/add"
+	pathRemoveApproval = "nft/approval/remove"
+)
+
+// Path returns the routing path for this message
+func (*AddApprovalMsg) Path() string {
+	return pathAddApproval
+}
+
+// Path returns the routing path for this message
+func (*RemoveApprovalMsg) Path() string {
+	return pathRemoveApproval
+}
+
+//TODO: Fields are exactly the same, except Add has ApprovalOptions. Shall we unify common
+// fields to baseApprovalMessage?
+func (m *AddApprovalMsg) Validate() error {
+	var validation *Validation
+	if err := weave.Address(m.Address).Validate(); err != nil {
+		return err
+	}
+	if !validation.IsValidAction(m.Action) {
+		return errors.ErrInternal("action must be valid")
+	}
+	if !validation.IsValidTokenID(m.Id) {
+		return errors.ErrInternal("id must be valid")
+	}
+	return nil
+}
+
+func (m *RemoveApprovalMsg) Validate() error {
+	var validation *Validation
+	if err := weave.Address(m.Address).Validate(); err != nil {
+		return err
+	}
+	if !validation.IsValidAction(m.Action) {
+		return errors.ErrInternal("action must be valid")
+	}
+	if !validation.IsValidTokenID(m.Id) {
+		return errors.ErrInternal("id must be valid")
+	}
+	return nil
+}
