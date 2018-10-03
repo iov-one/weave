52719d43e0ff01a88940e6f37b66dcc94d6b0688
Roman Useinov <roman.useinov@gmail.com>
2018-10-03 16:05:04 +0200

    add todo

--- x/nft/blockchain/msg.go
+++ x/nft/blockchain/msg.go
@@ -39,6 +39,9 @@ func (i *IssueTokenMsg) Validate() error {
 	if err := i.Details.Validate(); err != nil {
 		return err
 	}
+	//TODO: This is being validated on model save
+	//so in our case both check and deliver - double
+	//work?
 	if err := nft.NewApprovalOps(owner, &i.Approvals).
 		List().
 		Validate(); err != nil {
