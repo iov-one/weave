023c2a9f768ea61fe60b1ae8b3b91a4d7612ccb7
Roman Useinov <roman.useinov@gmail.com>
2018-10-03 16:03:23 +0200

    add findActor tests

---
+++ x/nft/helper_test.go
@@ -0,0 +1,59 @@
+package nft
+
+import (
+	"testing"
+
+	"github.com/iov-one/weave/x"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFindActor(t *testing.T) {
+	Convey("Test find actor", t, func() {
+		var helper x.TestHelpers
+		_, bob := helper.MakeKey()
+		_, alice := helper.MakeKey()
+		_, guest := helper.MakeKey()
+
+		Convey("Test owner", func() {
+			token := &NonFungibleToken{
+				Id:    []byte("asd"),
+				Owner: bob.Address(),
+			}
+			Convey("Test valid owner", func() {
+				addr := FindActor(helper.Authenticate(bob), nil, token, "")
+				So(addr, ShouldResemble, bob.Address())
+			})
+
+			Convey("Test invalid owner", func() {
+				addr := FindActor(helper.Authenticate(alice), nil, token, "")
+				So(addr, ShouldBeNil)
+			})
+		})
+
+		Convey("Test approvals", func() {
+			token := &NonFungibleToken{
+				Id:    []byte("asd"),
+				Owner: bob.Address(),
+				ActionApprovals: []ActionApprovals{{
+					Action:    Action_ActionUpdateDetails.String(),
+					Approvals: []Approval{{Options: ApprovalOptions{Count: UnlimitedCount}, Address: alice.Address()}},
+				}},
+			}
+			Convey("Test valid approval", func() {
+				addr := FindActor(helper.Authenticate(alice), nil, token, Action_ActionUpdateDetails.String())
+				So(addr, ShouldResemble, alice.Address())
+			})
+
+			Convey("Test invalid action", func() {
+				addr := FindActor(helper.Authenticate(alice), nil, token, Action_ActionUpdateApprovals.String())
+				So(addr, ShouldBeNil)
+			})
+
+			Convey("Test invalid signer", func() {
+				addr := FindActor(helper.Authenticate(guest), nil, token, "")
+				So(addr, ShouldBeNil)
+			})
+		})
+
+	})
+}
