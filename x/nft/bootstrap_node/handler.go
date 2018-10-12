26de9b5783b6ce922c94c69b76f1ca39188baec3
Roman Useinov <roman.useinov@gmail.com>
2018-10-12 11:23:03 +0200

    add bootstrap node token

---
+++ x/nft/bootstrap_node/handler.go
@@ -0,0 +1,91 @@
+package bootstrap_node
+
+import (
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
+	"github.com/iov-one/weave/x"
+	"github.com/iov-one/weave/x/nft"
+	"github.com/iov-one/weave/x/nft/blockchain"
+)
+
+const (
+	createNodeCost = 1
+)
+
+// RegisterRoutes will instantiate and register all handlers in this package
+func RegisterRoutes(r weave.Registry, auth x.Authenticator, issuer weave.Address) {
+	bucket := NewBucket()
+	r.Handle(pathIssueTokenMsg, NewIssueHandler(auth, issuer, bucket, blockchain.NewBucket()))
+}
+
+// RegisterQuery will register this bucket as "nft/bootstrap_nodes"
+func RegisterQuery(qr weave.QueryRouter) {
+	bucket := NewBucket()
+	bucket.Register("nft/bootstrap_nodes", qr)
+}
+
+type IssueHandler struct {
+	auth        x.Authenticator
+	issuer      weave.Address
+	bucket      Bucket
+	blockchains blockchain.Bucket
+}
+
+func NewIssueHandler(auth x.Authenticator, issuer weave.Address, bucket Bucket, blockchains blockchain.Bucket) *IssueHandler {
+	return &IssueHandler{auth: auth, issuer: issuer, bucket: bucket, blockchains: blockchains}
+}
+
+func (h IssueHandler) Check(ctx weave.Context, store weave.KVStore, tx weave.Tx) (weave.CheckResult, error) {
+	var res weave.CheckResult
+	if _, err := h.validate(ctx, tx); err != nil {
+		return res, err
+	}
+	res.GasAllocated += createNodeCost
+	return res, nil
+}
+
+func (h IssueHandler) Deliver(ctx weave.Context, store weave.KVStore, tx weave.Tx) (weave.DeliverResult, error) {
+	var res weave.DeliverResult
+	msg, err := h.validate(ctx, tx)
+	if err != nil {
+		return res, err
+	}
+	chain, err := h.blockchains.Get(store, msg.Details.BlockchainID)
+	switch {
+	case err != nil:
+		return res, err
+	case chain == nil:
+		return res, nft.ErrInvalidEntry()
+	}
+	o, err := h.bucket.Create(store, weave.Address(msg.Owner), msg.Id, msg.Approvals, msg.Details.BlockchainID, msg.Details.Uri)
+	if err != nil {
+		return res, err
+	}
+
+	return res, h.bucket.Save(store, o)
+}
+
+func (h IssueHandler) validate(ctx weave.Context, tx weave.Tx) (*IssueTokenMsg, error) {
+	rmsg, err := tx.GetMsg()
+	if err != nil {
+		return nil, err
+	}
+	msg, ok := rmsg.(*IssueTokenMsg)
+	if !ok {
+		return nil, errors.ErrUnknownTxType(rmsg)
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+	// check permissions
+	if h.issuer != nil {
+		if !h.auth.HasAddress(ctx, h.issuer) {
+			return nil, errors.ErrUnauthorized()
+		}
+	} else {
+		if !h.auth.HasAddress(ctx, msg.Owner) {
+			return nil, errors.ErrUnauthorized()
+		}
+	}
+	return msg, nil
+}
---
+++ x/nft/bootstrap_node/model.go
@@ -0,0 +1,106 @@
+package bootstrap_node
+
+import (
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
+	"github.com/iov-one/weave/orm"
+	"github.com/iov-one/weave/x/nft"
+	"github.com/iov-one/weave/x/nft/blockchain"
+)
+
+type Token interface {
+	nft.BaseNFT
+	GetBlockchainID() []byte
+	SetBlockchainID(actor weave.Address, id []byte) error
+	SetUri(actor weave.Address, uri string) error
+	GetUri() string
+
+
+}
+
+func (m *BootstrapNodeToken) OwnerAddress() weave.Address {
+	return m.Base.OwnerAddress()
+}
+
+func (m *BootstrapNodeToken) GetBlockchainID() []byte {
+	return m.Details.BlockchainID
+}
+
+func (m *BootstrapNodeToken) GetUri() string {
+	return m.Details.Uri
+}
+
+func (m *BootstrapNodeToken) Approvals() *nft.ApprovalOps {
+	return m.Base.Approvals()
+}
+
+func (m *BootstrapNodeToken) SetUri(actor weave.Address, uri string) error {
+	if !m.OwnerAddress().Equals(actor) {
+		panic("Not implemented, yet")
+		// TODO: handle permissions
+	}
+
+	m.Details.Uri = uri
+	return nil
+}
+
+func (m *BootstrapNodeToken) SetBlockchainID(actor weave.Address, id []byte) error {
+	if !m.OwnerAddress().Equals(actor) {
+		panic("Not implemented, yet")
+		// TODO: handle permissions
+	}
+
+	newID := make([]byte, len(id))
+	_ = copy(newID, id)
+
+	m.Details.BlockchainID = newID
+	return nil
+}
+
+func (m *BootstrapNodeToken) Transfer(newOwner weave.Address) error {
+	panic("implement me")
+}
+
+func (m *BootstrapNodeToken) Validate() error {
+	if err := m.Base.Validate(); err != nil {
+		return err
+	}
+	if err := m.Approvals().List().Validate(); err != nil {
+		return err
+	}
+	return m.Details.Validate()
+}
+
+func (m *BootstrapNodeToken) Copy() orm.CloneableData {
+	return &BootstrapNodeToken{
+		Base:    m.Base.Clone(),
+		Details: m.Details.Clone(),
+	}
+}
+
+func (m *TokenDetails) Clone() *TokenDetails {
+	// todo: impl
+	return &TokenDetails{BlockchainID: m.BlockchainID, Uri: m.Uri}
+}
+
+func (m *TokenDetails) Validate() error {
+	if m == nil {
+		return errors.ErrInternal("must not be nil")
+	}
+	if m.BlockchainID == nil || !blockchain.IsValidID(string(m.BlockchainID)) {
+		return nft.ErrInvalidEntry()
+	}
+	return nil
+}
+
+// AsNode will safely type-cast any value from Bucket
+func AsNode(obj orm.Object) (Token, error) {
+	if obj == nil || obj.Value() == nil {
+		return nil, nil
+	}
+	x, ok := obj.Value().(*BootstrapNodeToken)
+	if !ok {
+		return nil, nft.ErrUnsupportedTokenType()
+	}
+	return x, nil
+}
