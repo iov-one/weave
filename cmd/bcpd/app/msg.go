b0523815c5e78ce2ad3603b74ddf21395d46e3f8
Roman Useinov <roman.useinov@gmail.com>
2018-11-22 09:13:58 +0100

    add batch msg impl to bcpd

---
+++ cmd/bcpd/app/msg.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/x/batch"
+)
+
+var _ batch.Msg = (*BatchMsg)(nil)
+
+func (*BatchMsg) Path() string {
+	return batch.PathExecuteBatchMsg
+}
+
+func (msg *BatchMsg) MsgList() []weave.Msg {
+	messages := make([]weave.Msg, len(msg.Messages))
+	for _, m := range msg.Messages {
+		messages = append(messages, m.GetSum().(weave.Msg))
+	}
+	return messages
+}
