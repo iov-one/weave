23badc5054337197fb7c0ab9b0d27af0e7408c1a
Piotr Husiatyński <phusiatynski@gmail.com>
2018-12-19 13:21:43 +0100

    refactor tests using tendermint server (#243)
    Refactor tests that are using tendermint server to skip the test if the
    tendermint binary is not available.
    
    resolve #220

--- cmd/bnsd/commands/start_test.go
+++ cmd/bnsd/commands/start_test.go
@@ -1,20 +1,17 @@
 package commands
 
 import (
+	"context"
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	"github.com/tendermint/tendermint/libs/log"
-
 	"github.com/iov-one/weave/cmd/bnsd/app"
 	"github.com/iov-one/weave/commands/server"
+	"github.com/iov-one/weave/tmtest"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 func TestStartStandAlone(t *testing.T) {
@@ -40,41 +37,6 @@ func TestStartStandAlone(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestStartWithTendermint(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping Tendermint integration test")
-	}
-
-	home := setupConfig(t)
-	defer os.RemoveAll(home)
-
-	const runTime = 5     // how many seconds to run both processes
-	const startupTime = 2 // how many seconds to let tendermint startup
-
-	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout)).
-		With("module", "test-cmd")
-	err := server.InitCmd(app.GenInitOptions, logger, home, nil)
-	require.NoError(t, err)
-
-	// start up tendermint process in the background...
-	// this will block 2 seconds and ensure tendermint lives
-	// at least 3 seconds after we run StartCmd
-	runTendermint(t, home, startupTime, runTime)
-
-	// set up app and start up
-	args := []string{"-bind", "localhost:46658"}
-	runStart := func() error {
-		return server.StartCmd(app.GenerateApp, logger, home, args)
-	}
-	timeout := time.Duration(runTime+1) * time.Second
-	err = runOrTimeout(runStart, timeout)
-	require.NoError(t, err)
-
-	// give time for tendermint to shut down
-	fmt.Println(">>> Waiting for tendermint to shut down")
-	time.Sleep(time.Second + time.Second)
-}
-
 func runOrTimeout(cmd func() error, timeout time.Duration) error {
 	done := make(chan error)
 	go func(out chan<- error) {
@@ -95,41 +57,38 @@ func runOrTimeout(cmd func() error, timeout time.Duration) error {
 	}
 }
 
-// wait startupDelay before returning
-// fails if tendermint doesn't run at least startupDelay + timeout
-func runTendermint(t *testing.T, home string, startupDelay, timeout int64) {
-	tmBin := filepath.Join(
-		os.ExpandEnv("$GOPATH"), "bin", "tendermint")
-
-	// runTM should take longer than startupDelay and timeout...
-	runTm := func() error {
-		killTime := time.Duration(startupDelay + timeout + 2)
-
-		cmd := exec.Command(tmBin, "node", "--home", home)
-		fmt.Println(">>> Running tendermint...")
-
-		// log tendermint output for verbose debugging....
-		// cmd.Stdout = os.Stdout
-		// cmd.Stderr = os.Stdout
-
-		// run it
-		err := cmd.Start()
-		if err != nil {
-			return err
-		}
-
-		// after the given time, kill the process...
-		time.Sleep(killTime * time.Second)
-		fmt.Println("Killing tendermint...")
-		cmd.Process.Kill()
-		return nil
+func TestStartWithTendermint(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping Tendermint integration test")
 	}
 
-	runTime := time.Duration(timeout + startupDelay)
-	go func(t *testing.T) {
-		err := runOrTimeout(runTm, runTime*time.Second)
-		assert.NoError(t, err)
-	}(t)
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancel()
 
-	time.Sleep(time.Duration(startupDelay) * time.Second)
+	home := setupConfig(t)
+	defer os.RemoveAll(home)
+
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout)).
+		With("module", "test-cmd")
+	err := server.InitCmd(app.GenInitOptions, logger, home, nil)
+	require.NoError(t, err)
+
+	defer tmtest.RunTendermint(ctx, t, home)()
+
+	done := make(chan error, 1)
+	go func() {
+		args := []string{
+			"-bind", "localhost:46658",
+		}
+		done <- server.StartCmd(app.GenerateApp, logger, home, args)
+	}()
+
+	select {
+	case <-ctx.Done():
+		t.Logf("context cancelled before application finished")
+	case err := <-done:
+		if err != nil {
+			t.Fatalf("application failed: %s", err)
+		}
+	}
 }
