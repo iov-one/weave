9ba21cfef29cc9511158c1b3eb1b8f7a8193a970
Piotr Husiatyński <phusiatynski@gmail.com>
2019-01-03 14:17:52 +0100

    ensure multisig contract sequence start with 1

--- x/multisig/cmd/lsc/lsc.go
+++ x/multisig/cmd/lsc/lsc.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	offsetFl := flag.Int("offset", 0, "Ignore first N contract addresses")
-	limitFl := flag.Int("limit", 20, "Print N contract addresses")
+	offsetFl := flag.Int("offset", 1, "Ignore first N contract addresses.")
+	limitFl := flag.Int("limit", 20, "Print N contract addresses.")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, `Usage:
 	%s [options]
@@ -30,13 +30,22 @@ can create a reference to a contract before it exist.
 	}
 	flag.Parse()
 
+	if *offsetFl < 1 {
+		fmt.Fprintln(os.Stderr, "Offset must be greater than zero.")
+		os.Exit(2)
+	}
+	if *limitFl < 1 {
+		fmt.Fprintln(os.Stderr, "Limit must be greater than zero.")
+		os.Exit(2)
+	}
+
 	printAddresses(os.Stdout, *limitFl, *offsetFl)
 }
 
 func printAddresses(out io.Writer, limit, offset int) {
 	for i := offset; i < limit+offset; i++ {
 		cond := multisig.MultiSigCondition(seq(i))
-		fmt.Fprintf(out, "%d\t%s\n", i+1, cond.Address())
+		fmt.Fprintf(out, "%d\t%s\n", i, cond.Address())
 	}
 }
 
