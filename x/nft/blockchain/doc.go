1efd7c58cb4a2fcc6fbbbd93ff7c0fba09ce6d3b
Piotr Husiatyński <phusiatynski@gmail.com>
2019-01-17 13:18:24 +0100

    nft documentation (#267)
    * nft documentation
    
    Add doc.go and comments for protobuf declarations.
    
    * clearify meaning of the network_id variable

---
+++ x/nft/blockchain/doc.go
@@ -0,0 +1,7 @@
+/*
+
+Package blockchain provides an NFT implementation for registering blochchains
+together with their configuration.
+
+*/
+package blockchain
---
+++ x/nft/username/doc.go
@@ -0,0 +1,8 @@
+/*
+
+Package username provides an NFT implementation for maintaining a list of
+blockchain addresses for each user. For each user an address can be added or
+removed at any time.
+
+*/
+package username
