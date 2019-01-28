3f2122ea0fcd11c155100900cedf2eab4babebd6
Piotr Husiatyński <phusiatynski@gmail.com>
2019-01-28 16:44:45 +0100

    Initial global configuration package implementation
    `gconf` package implements a set of operations on a global
    configuration.
    
    Configuration is stored in a database with an interface compatible with
    `weave.KVStore`. Each value stored by configration is serialized using
    JSON.
    
    To load a configuration value, use one of the provided functions (`Int`,
    `String`, etc.). In case of parsing issues they log an error and return
    empty value for given type. This should not happen, unless database was
    manually altered.
    
    Next step is to add a support for runtime configuration update via
    message sending.
    
    resolve #202

---
+++ gconf/gconf.go
@@ -0,0 +1,61 @@
+package gconf
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/iov-one/weave"
+)
+
+type Store interface {
+	Get([]byte) []byte
+}
+
+func Int(confStore Store, propName string) int {
+	var value int
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func Duration(confStore Store, propName string) time.Duration {
+	var value time.Duration
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func String(confStore Store, propName string) string {
+	var value string
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func Strings(confStore Store, propName string) []string {
+	var value []string
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func Address(confStore Store, propName string) weave.Address {
+	var value weave.Address
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func Bytes(confStore Store, propName string) []byte {
+	value := make([]byte, 0, 128)
+	loadInto(confStore, propName, &value)
+	return value
+}
+
+func loadInto(confStore Store, propName string, dest interface{}) {
+	key := []byte("gconf:" + propName)
+	raw := confStore.Get(key)
+	if raw == nil {
+		log.Printf("cannot load %q configuration: not found", propName)
+		return
+	}
+	if err := json.Unmarshal(raw, dest); err != nil {
+		log.Printf("cannot load %q configuration: %s", propName, err)
+	}
+}
