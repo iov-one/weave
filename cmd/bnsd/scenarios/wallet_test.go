63517f5898b44a4cabc5c60a413b4712c5e67847
Alexander Peters <alpe@users.noreply.github.com>
2019-02-07 13:52:11 +0100

    Add test scenario with tx fee (#303)

--- cmd/bnsd/scenarios/wallet_test.go
+++ cmd/bnsd/scenarios/wallet_test.go
@@ -1,6 +1,7 @@
 package scenarios
 
 import (
+	"github.com/iov-one/weave/x/cash"
 	"testing"
 
 	"github.com/iov-one/weave/cmd/bnsd/client"
@@ -8,16 +9,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSendTokens(t *testing.T) {
+func TestSendTokensWithoutFee(t *testing.T) {
 	emilia := client.GenPrivateKey()
-	heights := make([]int64, 4)
 	aNonce := client.NewNonce(bnsClient, alice.PublicKey().Address())
 
 	walletResp, err := bnsClient.GetWallet(alice.PublicKey().Address())
 	require.NoError(t, err)
-	require.NotNil(t, walletResp)
+	require.NotNil(t, walletResp, "address not found")
 	require.NotEmpty(t, walletResp.Wallet.Coins)
 
+	heights := make([]int64, len(walletResp.Wallet.Coins))
 	for i, coin := range walletResp.Wallet.Coins {
 		// send a coin from Alice to Emilia
 		coin := x.Coin{
@@ -28,7 +29,47 @@ func TestSendTokens(t *testing.T) {
 
 		seq, err := aNonce.Next()
 		require.NoError(t, err)
-		tx := client.BuildSendTx(alice.PublicKey().Address(), emilia.PublicKey().Address(), coin, "text tx")
+		tx := client.BuildSendTx(alice.PublicKey().Address(), emilia.PublicKey().Address(), coin, "test tx without fee")
+		require.NoError(t, client.SignTx(tx, alice, chainID, seq))
+		resp := bnsClient.BroadcastTx(tx)
+		require.NoError(t, resp.IsError())
+		heights[i] = resp.Response.Height
+		delayForRateLimits()
+	}
+	walletResp, err = bnsClient.GetWallet(emilia.PublicKey().Address())
+	require.NoError(t, err)
+	t.Log("message", "done", "height", heights, "coins", walletResp.Wallet.Coins)
+}
+
+func TestSendTokenWithFee(t *testing.T) {
+	emilia := client.GenPrivateKey()
+	aNonce := client.NewNonce(bnsClient, alice.PublicKey().Address())
+
+	walletResp, err := bnsClient.GetWallet(alice.PublicKey().Address())
+	require.NoError(t, err)
+	require.NotNil(t, walletResp, "address not found")
+	require.NotEmpty(t, walletResp.Wallet.Coins)
+
+	heights := make([]int64, len(walletResp.Wallet.Coins))
+	for i, coin := range walletResp.Wallet.Coins {
+		// send a coin from Alice to Emilia
+		coin := x.Coin{
+			Ticker:     coin.Ticker,
+			Fractional: 0,
+			Whole:      1,
+		}
+
+		seq, err := aNonce.Next()
+		require.NoError(t, err)
+		tx := client.BuildSendTx(alice.PublicKey().Address(), emilia.PublicKey().Address(), coin, "test tx with fee")
+		tx.Fees = &cash.FeeInfo{
+			Payer: alice.PublicKey().Address(),
+			Fees: &x.Coin{
+				Ticker:     coin.Ticker,
+				Fractional: 1,
+				Whole:      0,
+			},
+		}
 		require.NoError(t, client.SignTx(tx, alice, chainID, seq))
 		resp := bnsClient.BroadcastTx(tx)
 		require.NoError(t, resp.IsError())
