e543f7002e57cdbef3d717baa040698528ad78c3
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-02-14 08:50:15 +0100

    Add getblock command to bnsd for loading blockchain info

--- cmd/bnsd/main.go
+++ cmd/bnsd/main.go
@@ -26,13 +26,14 @@ func init() {
 }
 
 func helpMessage() {
-	fmt.Println("bns")
-	fmt.Println("        Blockchain of Value node")
+	fmt.Println("bnsd")
+	fmt.Println("          Blockchain Name Service node")
 	fmt.Println("")
-	fmt.Println("help    Print this message")
-	fmt.Println("init    Initialize app options in genesis file")
-	fmt.Println("start   Run the abci server")
-	fmt.Println("version Print the app version")
+	fmt.Println("help      Print this message")
+	fmt.Println("init      Initialize app options in genesis file")
+	fmt.Println("start     Run the abci server")
+	fmt.Println("getblock  Extract a block from blockchain.db")
+	fmt.Println("version   Print the app version")
 	fmt.Println(`
   -home string
         directory to store files under (default "$HOME/.bns")`)
@@ -60,6 +61,8 @@ func main() {
 		err = server.InitCmd(app.GenInitOptions, logger, *varHome, rest)
 	case "start":
 		err = server.StartCmd(app.GenerateApp, logger, *varHome, rest)
+	case "getblock":
+		err = server.GetBlockCmd(logger, *varHome, rest)
 	case "testgen":
 		err = commands.TestGenCmd(app.Examples(), rest)
 	case "version":
---
+++ commands/server/getblock.go
@@ -0,0 +1,87 @@
+package server
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	amino "github.com/tendermint/go-amino"
+	"github.com/tendermint/tendermint/blockchain"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
+
+const (
+	flagHeight = "height"
+)
+
+var cdc = amino.NewCodec()
+
+func init() {
+	ctypes.RegisterAmino(cdc)
+}
+
+func parseArgs(args []string) (string, int64, error) {
+	if len(args) == 0 {
+		return "", 0, fmt.Errorf("Usage: cmd getblock <path to blockstore.db> [-height=H]")
+	}
+	var height int
+	getBlockFlags := flag.NewFlagSet("getblock", flag.ExitOnError)
+	getBlockFlags.IntVar(&height, flagHeight, 0, "height of the block to extract (default latest)")
+	err := getBlockFlags.Parse(args[1:])
+	return args[0], int64(height), err
+}
+
+// GetBlockCmd extracts a block from a blockstore.db and outputs as json
+// It takes the last block unless -height is explicitly specified
+// It writes the json to stdout
+func GetBlockCmd(logger log.Logger, home string, args []string) error {
+	dbPath, height, err := parseArgs(args)
+	if err != nil {
+		return err
+	}
+	db, err := openDb(dbPath)
+	if err != nil {
+		return err
+	}
+	store := blockchain.NewBlockStore(db)
+	if height == 0 {
+		height = store.Height()
+	}
+	return printBlock(store, height)
+}
+
+func openDb(dir string) (dbm.DB, error) {
+	if strings.HasSuffix(dir, ".db") {
+		dir = dir[:len(dir)-3]
+	} else if strings.HasSuffix(dir, ".db/") {
+		dir = dir[:len(dir)-4]
+	} else {
+		return nil, fmt.Errorf("Database directory must end with .db")
+	}
+	// TODO: doesn't work on windows!
+	cut := strings.LastIndex(dir, "/")
+	if cut == -1 {
+		return nil, fmt.Errorf("Cannot cut paths on %s", dir)
+	}
+	name := dir[cut+1:]
+	db, err := dbm.NewGoLevelDB(name, dir[:cut])
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func printBlock(store *blockchain.BlockStore, height int64) error {
+	block := store.LoadBlock(height)
+	if block == nil {
+		return fmt.Errorf("No block for height: %d", height)
+	}
+	js, err := cdc.MarshalJSONIndent(block, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(js))
+	return nil
+}
