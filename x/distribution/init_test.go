ed74004a0cac4ac17391afe7ef79614900659cdd
Piotr Husiatyński <phusiatynski@gmail.com>
2019-02-19 14:06:35 +0100

    extend genesis test

--- x/distribution/init_test.go
+++ x/distribution/init_test.go
@@ -16,13 +16,15 @@ func TestGenesisKey(t *testing.T) {
 				{
 					"admin": "E94323317C46BDA2268FA3698BAF4F95B893E8C7",
 					"recipients": [
-						{"weight": 2, "address": "E94323317C46BDA2268FA3698BAF4F95B893E8C7"}
+						{"weight": 2, "address": "E94323317C46BDA2268FA3698BAF4F95B893E8C7"},
+						{"weight": 1, "address": "FE5526DE08337DFEF5CF45EF3ED8C577B854DE34"}
 					]
 				}
 			]
 		}
 	`
-	addr, _ := hex.DecodeString("E94323317C46BDA2268FA3698BAF4F95B893E8C7")
+	addr1, _ := hex.DecodeString("E94323317C46BDA2268FA3698BAF4F95B893E8C7")
+	addr2, _ := hex.DecodeString("FE5526DE08337DFEF5CF45EF3ED8C577B854DE34")
 
 	var opts weave.Options
 	if err := json.Unmarshal([]byte(genesis), &opts); err != nil {
@@ -43,16 +45,22 @@ func TestGenesisKey(t *testing.T) {
 		t.Fatal("revenue not found")
 	}
 
-	if !rev.Admin.Equals(addr) {
+	if !rev.Admin.Equals(addr1) {
 		t.Fatalf("unexpected admin address: %q", rev.Admin)
 	}
-	if n := len(rev.Recipients); n != 1 {
+	if n := len(rev.Recipients); n != 2 {
 		t.Fatalf("expected one recipient, got %d", n)
 	}
 	if r := rev.Recipients[0]; r.Weight != 2 {
 		t.Fatalf("want weight 2, got %d", r.Weight)
 	}
-	if r := rev.Recipients[0]; !r.Address.Equals(addr) {
+	if r := rev.Recipients[0]; !r.Address.Equals(addr1) {
+		t.Fatalf("unexected address: %q", r.Address)
+	}
+	if r := rev.Recipients[1]; r.Weight != 1 {
+		t.Fatalf("want weight 1, got %d", r.Weight)
+	}
+	if r := rev.Recipients[1]; !r.Address.Equals(addr2) {
 		t.Fatalf("unexected address: %q", r.Address)
 	}
 }
