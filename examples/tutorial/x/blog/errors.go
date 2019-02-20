1800d55153035a8c5374bf67d11f3fd372922e1f
Roman Useinov <roman.useinov@gmail.com>
2019-02-20 19:07:52 +0100

    remove unused abci codes

--- examples/tutorial/x/blog/errors.go
+++ examples/tutorial/x/blog/errors.go
@@ -1,12 +1,5 @@
 package blog
 
-// ABCI Response Codes
-// tutorial reserves 400 ~ 420.
-const (
-	CodeNegativeNumber uint32 = 402
-	CodeInvalidBlog    uint32 = 403
-)
-
 var (
 	invalidTitle           = "title is too long or too short"
 	invalidText            = "text is too long or too short"
