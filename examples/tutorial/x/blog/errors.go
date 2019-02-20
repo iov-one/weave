4d0452427a255a9e9d076c1e3c4f430ffeea0f33
Roman Useinov <roman.useinov@gmail.com>
2019-02-20 19:07:40 +0100

    remove obsolete fields

--- examples/tutorial/x/blog/errors.go
+++ examples/tutorial/x/blog/errors.go
@@ -1,11 +1,5 @@
 package blog
 
-import (
-	"fmt"
-
-	"github.com/iov-one/weave/errors"
-)
-
 // ABCI Response Codes
 // tutorial reserves 400 ~ 420.
 const (
@@ -20,15 +14,4 @@ var (
 	descriptionTooLong     = "description is too long"
 	unauthorisedBlogAuthor = "blog author %X"
 	unauthorisedPostAuthor = "post author %X"
-
-	errNegativeArticles = fmt.Errorf("Article count is negative")
-	errNegativeCreation = fmt.Errorf("Creation block is negative")
-
-	errBlogNotFound      = fmt.Errorf("No blog found for post")
-	errBlogExist         = fmt.Errorf("Blog already exists")
-	errBlogOneAuthorLeft = fmt.Errorf("Unable to remove last blog author")
 )
-
-func ErrBlogOneAuthorLeft() error {
-	return errors.WithCode(errBlogOneAuthorLeft, CodeInvalidBlog)
-}
