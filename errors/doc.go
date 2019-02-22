dd3080e99c4d9f3d58b12927248e66a9d1916b86
Roman Useinov <roman.useinov@gmail.com>
2019-02-22 05:07:23 +0100

    doc

---
+++ errors/doc.go
@@ -0,0 +1,17 @@
+/**
+ Package errors implements custom error interfaces for weave.
+
+  The idea is to reuse as many errors from this package as possible and define custom package
+  errors when absolutely necessary. It is best to define a new error here if you feel it's going to
+  be somewhat package-agnostic.
+
+  x/multisig is a good package to take a look at in terms of usage with predefined strings with/without formatting.
+  x/validators and x/sigs define some custom errors.
+
+  If you want to register a custom error - use Register(code, description).
+  For reusing errors - use Errxxx.New and Errxxx.Newf.
+
+  Also, error package defines a convenient Is helper to compare errors as well as each err defines an Is
+  helper to compare errors directly to that type.
+ */
+package errors
