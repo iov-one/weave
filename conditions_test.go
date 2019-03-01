b60341a69e23b55688f77dfcd491eb82f130bd3a
Piotr Husiatyński <phusiatynski@gmail.com>
2019-03-01 14:43:10 +0100

    allow more formats for JSON serialized address
    In addition to a usual hex encoded address, when deserializing from JSON
    allow for more formats.
    
    resolve #347

--- conditions_test.go
+++ conditions_test.go
@@ -1,10 +1,13 @@
 package weave_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -21,4 +24,54 @@ func TestAddressPrinting(t *testing.T) {
 
 		So(cond.String(), ShouldNotEqual, fmt.Sprintf("%X", cond))
 	})
+}
+
+func TestAddressUnmarshalJSON(t *testing.T) {
+	cases := map[string]struct {
+		json     string
+		wantErr  error
+		wantAddr weave.Address
+	}{
+		"default decoding": {
+			json:     `"6865782d61646472"`,
+			wantAddr: weave.Address("hex-addr"),
+		},
+		"hex decoding": {
+			json:     `"hex:6865782d61646472"`,
+			wantAddr: weave.Address("hex-addr"),
+		},
+		"cond decoding": {
+			json:     `"cond:foo/bar/12:3"`,
+			wantAddr: weave.NewCondition("foo", "bar", []byte("12:3")).Address(),
+		},
+		"unknown format": {
+			json:    `"foobar:xxx"`,
+			wantErr: errors.ErrInvalidType,
+		},
+		"zero address": {
+			json:     `""`,
+			wantAddr: nil,
+		},
+		"zero hex address": {
+			json:     `"hex:"`,
+			wantAddr: nil,
+		},
+		"zero cond address": {
+			json:     `"cond:"`,
+			wantAddr: nil,
+		},
+	}
+
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			var a weave.Address
+			err := json.Unmarshal([]byte(tc.json), &a)
+			if !errors.Is(tc.wantErr, err) {
+				t.Fatalf("got error: %+v", err)
+			}
+			if err == nil && !reflect.DeepEqual(a, tc.wantAddr) {
+				t.Fatalf("got address: %q", a)
+			}
+		})
+	}
 }
