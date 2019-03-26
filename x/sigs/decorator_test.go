46b8243738ee03491b8a6d22363c7ba361025947
Piotr Husiatyński <phusiatynski@gmail.com>
2019-03-26 09:09:52 +0100

    Charge gas for signature verification.
    For each successfully verified signature in `x/sigs` decorator change a
    fee.
    
    resolve #429

--- x/sigs/decorator_test.go
+++ x/sigs/decorator_test.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
 	"github.com/iov-one/weave"
 	"github.com/iov-one/weave/crypto"
 	"github.com/iov-one/weave/store"
+	"github.com/iov-one/weave/weavetest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDecorator(t *testing.T) {
@@ -72,8 +72,6 @@ func TestDecorator(t *testing.T) {
 
 }
 
-//---------------- helpers --------
-
 // SigCheckHandler stores the seen signers on each call
 type SigCheckHandler struct {
 	Signers []weave.Condition
@@ -91,4 +89,31 @@ func (s *SigCheckHandler) Deliver(ctx weave.Context, store weave.KVStore,
 	tx weave.Tx) (res weave.DeliverResult, err error) {
 	s.Signers = Authenticate{}.GetConditions(ctx)
 	return
+}
+
+func TestGasPaymentPerSigner(t *testing.T) {
+	var (
+		h weavetest.Handler
+		d Decorator
+	)
+
+	ctx := context.Background()
+	ctx = weave.WithChainID(ctx, "mychain")
+	db := store.MemStore()
+
+	priv := weavetest.NewKey()
+	tx := NewStdTx([]byte("foo"))
+	if sig, err := SignTx(priv, tx, "mychain", 0); err != nil {
+		t.Fatalf("cannot sign the transaction: %s", err)
+	} else {
+		tx.Signatures = []*StdSignature{sig}
+	}
+
+	res, err := d.Check(ctx, db, tx, &h)
+	if err != nil {
+		t.Fatalf("cannot check: %s", err)
+	}
+	if got, want := res.GasPayment, int64(signatureVerifyCost); want != got {
+		t.Fatalf("want %d gas payment, got %d", want, got)
+	}
 }
