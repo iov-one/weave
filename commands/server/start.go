3cff7398f9c5a0baf8aaf7467f4cc030cdccc4c9
Roman Useinov <roman.useinov@gmail.com>
2019-03-28 13:47:52 +0100

    Add a fee flag

--- commands/server/start.go
+++ commands/server/start.go
@@ -2,9 +2,9 @@ package server
 
 import (
 	"flag"
+	"fmt"
 
-	"github.com/pkg/errors"
-
+	"github.com/iov-one/weave/coin"
 	"github.com/tendermint/tendermint/abci/server"
 	abci "github.com/tendermint/tendermint/abci/types"
 	cmn "github.com/tendermint/tendermint/libs/common"
@@ -12,19 +12,31 @@ import (
 )
 
 const (
-	flagBind  = "bind"
-	flagDebug = "debug"
+	flagBind   = "bind"
+	flagDebug  = "debug"
+	flagMinFee = "min_fee"
 )
 
-func parseFlags(args []string) (string, bool, error) {
+func parseFlags(args []string) (string, bool, coin.Coin, error) {
 	// parse flagBind and return the result
 	var addr string
 	var debug bool
+	var minFeeStr string
+	minFee := coin.Coin{}
+
 	startFlags := flag.NewFlagSet("start", flag.ExitOnError)
 	startFlags.StringVar(&addr, flagBind, "tcp://localhost:46658", "address server listens on")
+	startFlags.StringVar(&minFeeStr, flagMinFee, "0 IOV", "minimal anti-spam fee")
 	startFlags.BoolVar(&debug, flagDebug, false, "call stack returned on error")
 	err := startFlags.Parse(args)
-	return addr, debug, err
+
+	if err != nil {
+		return addr, debug, minFee, err
+	}
+
+	err = minFee.UnmarshalJSON([]byte(minFeeStr))
+
+	return addr, debug, minFee, err
 }
 
 // AppGenerator lets us lazily initialize app, using home dir
@@ -33,7 +45,7 @@ type AppGenerator func(string, log.Logger, bool) (abci.Application, error)
 
 // StartCmd initializes the application, and
 func StartCmd(gen AppGenerator, logger log.Logger, home string, args []string) error {
-	addr, debug, err := parseFlags(args)
+	addr, debug, minFee, err := parseFlags(args)
 	if err != nil {
 		return err
 	}
@@ -48,7 +60,7 @@ func StartCmd(gen AppGenerator, logger log.Logger, home string, args []string) e
 
 	svr, err := server.NewServer(addr, "socket", app)
 	if err != nil {
-		return errors.Errorf("Error creating listener: %v\n", err)
+		return fmt.Errorf("Error creating listener: %v\n", err)
 	}
 	svr.SetLogger(logger.With("module", "abci-server"))
 	svr.Start()
