12e985bcf9ec5709b0092adf605db0b99c2fd9c0
Piotr Husiatyński <phusiatynski@gmail.com>
2019-04-03 11:24:19 +0200

    x/multisig: cleanup tests
    part of #249

--- x/multisig/context_test.go
+++ x/multisig/context_test.go
@@ -2,73 +2,77 @@ package multisig
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/iov-one/weave"
 	"github.com/iov-one/weave/weavetest"
-	"github.com/stretchr/testify/assert"
+	"github.com/iov-one/weave/weavetest/assert"
 )
 
 func TestContext(t *testing.T) {
-	// sig is a signature permission for contractID, not a contract ID
-	contractID := weavetest.SequenceID(1)
-	sig := MultiSigCondition(contractID).Address()
+	contract1 := weavetest.SequenceID(1)
+	sig1 := MultiSigCondition(contract1).Address()
 
-	// other is a signature permission for some "other" contract ID
-	otherContractID := weavetest.SequenceID(2)
-	other := MultiSigCondition(otherContractID).Address()
+	contract2 := weavetest.SequenceID(2)
+	sig2 := MultiSigCondition(contract2).Address()
 
-	// random address which does not represent anything in particular
-	random := weave.NewAddress(weavetest.SequenceID(3))
+	contract3 := weave.NewAddress(weavetest.SequenceID(3))
+	sig3 := MultiSigCondition(contract3).Address()
 
 	bg := context.Background()
-	cases := []struct {
-		ctx   weave.Context
-		perms []weave.Condition
-		match []weave.Address
-		not   []weave.Address
+	cases := map[string]struct {
+		ctx        weave.Context
+		wantPerms  []weave.Condition
+		wantAddr   []weave.Address
+		wantNoAddr []weave.Address
 	}{
-		{bg, nil, nil, []weave.Address{sig, other, random}},
-		{
-			withMultisig(bg, contractID),
-			[]weave.Condition{MultiSigCondition(contractID)},
-			[]weave.Address{sig},
-			[]weave.Address{other, random},
+		"empty context": {
+			ctx:        bg,
+			wantNoAddr: []weave.Address{sig1, sig2, contract3},
 		},
-		{
-			withMultisig(bg, otherContractID),
-			[]weave.Condition{MultiSigCondition(otherContractID)},
-			[]weave.Address{other},
-			[]weave.Address{sig, random},
+		"context with a single contract": {
+			ctx: withMultisig(bg, contract1),
+			wantPerms: []weave.Condition{
+				MultiSigCondition(contract1),
+			},
+			wantAddr:   []weave.Address{sig1},
+			wantNoAddr: []weave.Address{sig2, contract3, sig3},
 		},
-		{
-			// add multisig conditions for both contractID and otherContractID to the context
-			withMultisig(withMultisig(bg, contractID), otherContractID),
-			[]weave.Condition{MultiSigCondition(contractID), MultiSigCondition(otherContractID)},
-			[]weave.Address{sig, other},
-			[]weave.Address{random},
+		"context with a another single contract": {
+			ctx: withMultisig(bg, contract2),
+			wantPerms: []weave.Condition{
+				MultiSigCondition(contract2),
+			},
+			wantAddr:   []weave.Address{sig2},
+			wantNoAddr: []weave.Address{sig1, contract3, sig3},
 		},
-		{
-			withMultisig(bg, weavetest.SequenceID(3)),
-			[]weave.Condition{MultiSigCondition(weavetest.SequenceID(3))},
-			nil,
-			[]weave.Address{sig, other, random},
+		"context with two contracts": {
+			ctx: withMultisig(withMultisig(bg, contract1), contract2),
+			wantPerms: []weave.Condition{
+				MultiSigCondition(contract1),
+				MultiSigCondition(contract2),
+			},
+			wantAddr:   []weave.Address{sig1, sig2},
+			wantNoAddr: []weave.Address{contract3, sig3},
 		},
 	}
 
-	auth := Authenticate{}
-	for i, tc := range cases {
-		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
-			perms := auth.GetConditions(tc.ctx)
-			assert.Equal(t, tc.perms, perms)
+	var auth Authenticate
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			wantPerms := auth.GetConditions(tc.ctx)
+			assert.Equal(t, tc.wantPerms, wantPerms)
 
-			for _, a := range tc.match {
-				assert.True(t, auth.HasAddress(tc.ctx, a))
+			for _, a := range tc.wantAddr {
+				if !auth.HasAddress(tc.ctx, a) {
+					t.Errorf("missing address: %q", a)
+				}
 			}
 
-			for _, a := range tc.not {
-				assert.False(t, auth.HasAddress(tc.ctx, a))
+			for _, a := range tc.wantNoAddr {
+				if auth.HasAddress(tc.ctx, a) {
+					t.Errorf("unexpected address: %q", a)
+				}
 			}
 		})
 	}
