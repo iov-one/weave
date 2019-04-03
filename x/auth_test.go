66f31b4315eb17dda64dc995652013f6ca6cfab1
Piotr Husiatyński <phusiatynski@gmail.com>
2019-04-03 12:12:26 +0200

    weave/x: cleanup tests
    part of #429

--- x/auth_test.go
+++ x/auth_test.go
@@ -2,12 +2,11 @@ package x
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/iov-one/weave"
 	"github.com/iov-one/weave/weavetest"
-	"github.com/stretchr/testify/assert"
+	"github.com/iov-one/weave/weavetest/assert"
 )
 
 func TestAuth(t *testing.T) {
@@ -18,76 +17,80 @@ func TestAuth(t *testing.T) {
 	ctx1 := &weavetest.CtxAuth{Key: "foo"}
 	ctx2 := &weavetest.CtxAuth{Key: "bar"}
 
-	cases := []struct {
-		ctx        weave.Context
-		auth       Authenticator
-		mainSigner weave.Condition
-		has        weave.Condition
-		notHave    weave.Condition
-		all        []weave.Condition
+	cases := map[string]struct {
+		ctx          weave.Context
+		auth         Authenticator
+		mainSigner   weave.Condition
+		wantInCtx    weave.Condition
+		wantNotInCtx weave.Condition
+		wantAll      []weave.Condition
 	}{
-		0: {
-			context.Background(),
-			&weavetest.Auth{},
-			nil,
-			nil,
-			b,
-			nil,
+		"empty context": {
+			ctx:          context.Background(),
+			auth:         &weavetest.Auth{},
+			wantNotInCtx: b,
 		},
-		{
-			context.Background(),
-			&weavetest.Auth{Signer: a},
-			a,
-			a,
-			b,
-			[]weave.Condition{a},
+		"signer a": {
+			ctx:          context.Background(),
+			auth:         &weavetest.Auth{Signer: a},
+			mainSigner:   a,
+			wantInCtx:    a,
+			wantNotInCtx: b,
+			wantAll:      []weave.Condition{a},
 		},
-		{
-			context.Background(),
-			ChainAuth(
+		"signer b": {
+			ctx: context.Background(),
+			auth: ChainAuth(
 				&weavetest.Auth{Signer: b},
 				&weavetest.Auth{Signer: a}),
-			b,
-			b,
-			c,
-			[]weave.Condition{b, a},
+			mainSigner:   b,
+			wantInCtx:    b,
+			wantNotInCtx: c,
+			wantAll:      []weave.Condition{b, a},
 		},
-		// ctxAuth checks what is set by same key
-		{
-			ctx1.SetConditions(context.Background(), a, b),
-			ctx1,
-			a,
-			b,
-			c,
-			[]weave.Condition{a, b},
+		"ctxAuth checks what is set by same key": {
+			ctx:          ctx1.SetConditions(context.Background(), a, b),
+			auth:         ctx1,
+			mainSigner:   a,
+			wantInCtx:    b,
+			wantNotInCtx: c,
+			wantAll:      []weave.Condition{a, b},
 		},
-		// ctxAuth with different key sees nothing
-		{
-			ctx1.SetConditions(context.Background(), a, b),
-			ctx2,
-			nil,
-			nil,
-			a,
-			nil,
+		"ctxAuth with different key sees nothing": {
+			ctx:          ctx1.SetConditions(context.Background(), a, b),
+			auth:         ctx2,
+			wantNotInCtx: a,
 		},
 	}
 
-	for i, tc := range cases {
-		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
-			ctx := tc.ctx
-			assert.Equal(t, tc.mainSigner, MainSigner(ctx, tc.auth))
-			if tc.has != nil {
-				assert.True(t, tc.auth.HasAddress(ctx, tc.has.Address()))
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			assert.Equal(t, tc.mainSigner, MainSigner(tc.ctx, tc.auth))
+			if tc.wantInCtx != nil && !tc.auth.HasAddress(tc.ctx, tc.wantInCtx.Address()) {
+				t.Fatal("condition address that was expected in context not found")
 			}
-			assert.False(t, tc.auth.HasAddress(ctx, tc.notHave.Address()))
 
-			all := tc.auth.GetConditions(ctx)
-			assert.Equal(t, tc.all, all)
-			assert.True(t, HasAllConditions(ctx, tc.auth, all))
-			assert.False(t, HasAllConditions(ctx, tc.auth, append(all, tc.notHave)))
+			if tc.wantNotInCtx != nil && tc.auth.HasAddress(tc.ctx, tc.wantNotInCtx.Address()) {
+				t.Fatal("conditino address that was expected not to be in context found")
+			}
+
+			all := tc.auth.GetConditions(tc.ctx)
+			assert.Equal(t, tc.wantAll, all)
+
+			if !HasAllConditions(tc.ctx, tc.auth, all) {
+				t.Fatal("has all conditions check failed")
+			}
+			if HasAllConditions(tc.ctx, tc.auth, append(all, tc.wantNotInCtx)) {
+				t.Fatal("has all condition succeded after adding non existing condition")
+			}
+
 			if len(all) > 0 {
-				assert.True(t, HasNConditions(ctx, tc.auth, all, len(all)-1))
-				assert.False(t, HasNConditions(ctx, tc.auth, all, len(all)+1))
+				if !HasNConditions(tc.ctx, tc.auth, all, len(all)-1) {
+					t.Fatal("want condition check of a subset to succeed")
+				}
+				if HasNConditions(tc.ctx, tc.auth, all, len(all)+1) {
+					t.Fatal("want condition check of a superset to fail")
+				}
 			}
 		})
 	}
--- x/doc_test.go
+++
@@ -1,8 +0,0 @@
-package x
-
-import "testing"
-
-// TestCoverHappy just makes sure that coverage doesn't
-// print errors on this package.
-// remove if there is any thing to test
-func TestCoverHappy(t *testing.T) {}
