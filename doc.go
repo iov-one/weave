20f6a4adac8e951bfa158325f107f0ca39b1d005
Roman Useinov <roman.useinov@gmail.com>
2019-04-04 18:59:20 +0200

    Add documentation for weave package

---
+++ doc.go
@@ -0,0 +1,5 @@
+package weave
+/*
+Package weave defines interfaces used throughout the app, such as: storage, transactions, handlers etc.
+It also contains helpers to work with errors, context, authentication and abci.
+ */
