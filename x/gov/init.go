e4544dec710775521d6a0406b9c431da85034663
Alexander Peters <alpe@users.noreply.github.com>
2019-04-05 10:05:11 +0200

    Governance initializers (#462)
    * Initial version of gov initializer
    
    * Rework test to stdlib conditions
    
    * Additional initializer tests
    
    * Validation rules proposal
    
    * Review comments
    
    * Limit number of participants

---
+++ x/gov/init.go
@@ -0,0 +1,78 @@
+package gov
+
+import (
+	"fmt"
+
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
+)
+
+// Initializer fulfils the Initializer interface to load data from the genesis
+// file
+type Initializer struct{}
+
+var _ weave.Initializer = (*Initializer)(nil)
+
+// FromGenesis will parse initial governance electorate and election rules from genesis
+// and saves it in the database.
+func (*Initializer) FromGenesis(opts weave.Options, db weave.KVStore) error {
+	var governance struct {
+		Electorate []struct {
+			Title        string `json:"title"`
+			Participants []struct {
+				Signature weave.Address `json:"signature"`
+				Weight    uint32        `json:"weight"`
+			} `json:"participants"`
+		} `json:"electorate"`
+		Rules []struct {
+			Title             string `json:"title"`
+			VotingPeriodHours uint32 `json:"voting_period_hours"`
+			Fraction          struct {
+				Numerator   uint32 `json:"numerator"`
+				Denominator uint32 `json:"denominator"`
+			} `json:"fraction"`
+		} `json:"rules"`
+	}
+	if err := opts.ReadOptions("governance", &governance); err != nil {
+		return err
+	}
+	// handle electorate
+	electBucket := NewElectorateBucket()
+	for i, e := range governance.Electorate {
+		ps := make([]Participant, 0, len(e.Participants))
+		for _, p := range e.Participants {
+			ps = append(ps, Participant{
+				Signature: p.Signature,
+				Weight:    p.Weight,
+			})
+		}
+		electorate := Electorate{
+			Title:        e.Title,
+			Participants: ps,
+		}
+		if err := electorate.Validate(); err != nil {
+			return errors.Wrap(err, fmt.Sprintf("electorate #%d is invalid", i))
+		}
+		obj := electBucket.Build(db, &electorate)
+		if err := electBucket.Save(db, obj); err != nil {
+			return err
+		}
+	}
+	// handle election rules
+	rulesBucket := NewElectionRulesBucket()
+	for i, r := range governance.Rules {
+		rule := ElectionRule{
+			Title:             r.Title,
+			VotingPeriodHours: r.VotingPeriodHours,
+			Threshold:         Fraction{Numerator: r.Fraction.Numerator, Denominator: r.Fraction.Denominator},
+		}
+		if err := rule.Validate(); err != nil {
+			return errors.Wrap(err, fmt.Sprintf("eletionRule #%d is invalid", i))
+		}
+		obj := rulesBucket.Build(db, &rule)
+		if err := rulesBucket.Save(db, obj); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ x/gov/init_test.go
@@ -0,0 +1,150 @@
+package gov
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"testing"
+
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/store"
+	"github.com/iov-one/weave/weavetest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInitFromGenesis(t *testing.T) {
+	const genesisSnippet = `
+{
+  "governance": {
+    "electorate": [
+      {
+        "title": "first",
+        "participants": [
+          {
+            "weight": 10,
+            "signature": "1111111111111111111111111111111111111111"
+          },
+          {
+            "weight": 11,
+            "signature": "2222222222222222222222222222222222222222"
+          }
+        ]
+      },
+      {
+        "title": "second",
+        "participants": [
+          {
+            "weight": 1,
+            "signature": "3333333333333333333333333333333333333333"
+          }
+        ]
+      }
+    ],
+    "rules": [
+      {
+        "title": "fooo",
+        "voting_period_hours": 1,
+        "fraction": {
+          "numerator": 2,
+          "denominator": 3
+        }
+      },
+      {
+        "title": "barr",
+        "voting_period_hours": 2,
+        "fraction": {
+          "numerator": 1,
+          "denominator": 2
+        }
+      }
+    ]
+  }
+}`
+	var opts weave.Options
+	require.NoError(t, json.Unmarshal([]byte(genesisSnippet), &opts))
+
+	db := store.MemStore()
+	// when
+	var ini Initializer
+	if err := ini.FromGenesis(opts, db); err != nil {
+		t.Fatalf("cannot load genesis: %s", err)
+	}
+	// then
+	// first electorate ok
+	e, err := NewElectorateBucket().GetElectorate(db, weavetest.SequenceID(1))
+	if err != nil || e == nil {
+		t.Fatalf("unexpected result: error: %s", err)
+	}
+	if exp, got := "first", e.Title; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := 2, len(e.Participants); exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := addr("1111111111111111111111111111111111111111"), e.Participants[0].Signature; !exp.Equals(got) {
+		t.Errorf("expected %X but got %X", exp, got)
+	}
+	if exp, got := uint32(10), e.Participants[0].Weight; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := addr("2222222222222222222222222222222222222222"), e.Participants[1].Signature; !exp.Equals(got) {
+		t.Errorf("expected %X but got %X", exp, got)
+	}
+	if exp, got := uint32(11), e.Participants[1].Weight; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	// second electorate ok
+	e, err = NewElectorateBucket().GetElectorate(db, weavetest.SequenceID(2))
+	if err != nil || e == nil {
+		t.Fatalf("unexpected result: error: %s", err)
+	}
+	if exp, got := "second", e.Title; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := 1, len(e.Participants); exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := addr("3333333333333333333333333333333333333333"), e.Participants[0].Signature; !exp.Equals(got) {
+		t.Errorf("expected %X but got %X", exp, got)
+	}
+	if exp, got := uint32(1), e.Participants[0].Weight; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+
+	// and then
+	// first election rule ok
+	r, err := NewElectionRulesBucket().GetElectionRule(db, weavetest.SequenceID(1))
+	if err != nil || r == nil {
+		t.Fatalf("unexpected result: error: %s", err)
+	}
+	if got, exp := "fooo", r.Title; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := uint32(1), r.VotingPeriodHours; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := (Fraction{Numerator: 2, Denominator: 3}), r.Threshold; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	// second election rule ok
+	r, err = NewElectionRulesBucket().GetElectionRule(db, weavetest.SequenceID(2))
+	if err != nil || r == nil {
+		t.Fatalf("unexpected result: error: %s", err)
+	}
+	if got, exp := "barr", r.Title; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := uint32(2), r.VotingPeriodHours; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+	if exp, got := (Fraction{Numerator: 1, Denominator: 2}), r.Threshold; exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+}
+
+func addr(s string) weave.Address {
+	a, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return a
+}
