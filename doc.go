ff6746498a8f78facbe480dfabf32a76fd5c53cb
Roman Useinov <roman.useinov@gmail.com>
2019-04-06 16:50:27 +0200

    Weave package doc.go (#493)
    * Add documentation for weave package
    
    * More weave package documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+package weave
+/*
+Package weave defines interfaces used throughout the app, such as: storage, transactions, handlers etc.
+It also contains helpers to work with errors, context, authentication and abci.
+Look into this package to get an brief overview of design decisions made around interfaces and extension
+building blocks.
+ */
