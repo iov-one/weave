114130a0f6c32bf56e707754fb76cae2a1f0c97a
Alexander Peters <alpe@users.noreply.github.com>
2019-04-08 12:23:49 +0200

    Election tally (#497)
    * Tally message and handling
    
    * Tag successful tally message

--- x/gov/handler.go
+++ x/gov/handler.go
@@ -4,12 +4,18 @@ import (
 	"github.com/iov-one/weave"
 	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/x"
+	"github.com/tendermint/tendermint/libs/common"
 )
 
 const (
 	newTextProposalCost = 0
 	voteCost            = 0
 	tallyCost           = 0
+)
+
+const (
+	tagProposerID = "proposal-id"
+	tagAction     = "action"
 )
 
 // RegisterQuery registers governance buckets for querying.
@@ -27,6 +33,10 @@ func RegisterRoutes(r weave.Registry, auth x.Authenticator) {
 		auth:       auth,
 		propBucket: propBucket,
 		elecBucket: elecBucket,
+	})
+	r.Handle(pathTallyMsg, &TallyHandler{
+		auth:   auth,
+		bucket: propBucket,
 	})
 }
 
@@ -97,4 +107,60 @@ func (h VoteHandler) validate(ctx weave.Context, db weave.KVStore, tx weave.Tx) 
 		return nil, nil, nil, errors.Wrap(errors.ErrUnauthorized, "not in participants list")
 	}
 	return &msg, proposal, elector, nil
+}
+
+type TallyHandler struct {
+	auth   x.Authenticator
+	bucket *ProposalBucket
+}
+
+func (h TallyHandler) Check(ctx weave.Context, db weave.KVStore, tx weave.Tx) (weave.CheckResult, error) {
+	var res weave.CheckResult
+	if _, _, err := h.validate(ctx, db, tx); err != nil {
+		return res, err
+	}
+	res.GasAllocated += tallyCost
+	return res, nil
+
+}
+
+func (h TallyHandler) Deliver(ctx weave.Context, db weave.KVStore, tx weave.Tx) (weave.DeliverResult, error) {
+	var res weave.DeliverResult
+	msg, proposal, err := h.validate(ctx, db, tx)
+	if err != nil {
+		return res, err
+	}
+	if err := proposal.Tally(); err != nil {
+		return res, err
+	}
+	if err := h.bucket.Update(db, msg.ProposalId, proposal); err != nil {
+		return res, err
+	}
+	res.Tags = append(res.Tags, []common.KVPair{
+		{Key: []byte(tagProposerID), Value: msg.ProposalId},
+		{Key: []byte(tagAction), Value: []byte("tally")},
+	}...)
+	return res, nil
+}
+
+func (h TallyHandler) validate(ctx weave.Context, db weave.KVStore, tx weave.Tx) (*TallyMsg, *TextProposal, error) {
+	var msg TallyMsg
+	if err := weave.LoadMsg(tx, &msg); err != nil {
+		return nil, nil, errors.Wrap(err, "load msg")
+	}
+	proposal, err := h.bucket.GetTextProposal(db, msg.ProposalId)
+	if err != nil {
+		return nil, nil, err
+	}
+	if proposal.Status != TextProposal_Undefined {
+		return nil, nil, errors.Wrap(errors.ErrInvalidState, "tally executed before")
+	}
+	blockTime, ok := weave.BlockTime(ctx)
+	if !ok {
+		return nil, nil, errors.Wrap(errors.ErrHuman, "block time not set")
+	}
+	if !blockTime.After(proposal.VotingEndTime.Time()) {
+		return nil, nil, errors.Wrap(errors.ErrInvalidState, "tally before proposal end time")
+	}
+	return &msg, proposal, nil
 }
--- x/gov/handler_test.go
+++ x/gov/handler_test.go
@@ -2,6 +2,8 @@ package gov
 
 import (
 	"context"
+	"math"
+	"reflect"
 	"testing"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/store"
 	"github.com/iov-one/weave/weavetest"
+	"github.com/tendermint/tendermint/libs/common"
 )
 
 var (
@@ -44,44 +47,50 @@ func TestVote(t *testing.T) {
 			Exp: TallyResult{TotalYes: 1},
 		},
 		"Vote with invalid option": {
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Invalid, Voter: alice},
-			WantCheckErr: errors.ErrInvalidInput,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Invalid, Voter: alice},
+			WantCheckErr:   errors.ErrInvalidInput,
+			WantDeliverErr: errors.ErrInvalidInput,
 		},
 		"Unauthorized voter": {
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: weavetest.NewCondition().Address()},
-			WantCheckErr: errors.ErrUnauthorized,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: weavetest.NewCondition().Address()},
+			WantCheckErr:   errors.ErrUnauthorized,
+			WantDeliverErr: errors.ErrUnauthorized,
 		},
 		"Vote before start date": {
 			Mods: func(ctx weave.Context, proposal *TextProposal) {
 				blockTime, _ := weave.BlockTime(ctx)
 				proposal.VotingStartTime = weave.AsUnixTime(blockTime.Add(time.Second))
 			},
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
-			WantCheckErr: errors.ErrInvalidState,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
 		},
 		"Vote on start date": {
 			Mods: func(ctx weave.Context, proposal *TextProposal) {
 				blockTime, _ := weave.BlockTime(ctx)
 				proposal.VotingStartTime = weave.AsUnixTime(blockTime)
 			},
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
-			WantCheckErr: errors.ErrInvalidState,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
 		},
 		"Vote on end date": {
 			Mods: func(ctx weave.Context, proposal *TextProposal) {
 				blockTime, _ := weave.BlockTime(ctx)
 				proposal.VotingEndTime = weave.AsUnixTime(blockTime)
 			},
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
-			WantCheckErr: errors.ErrInvalidState,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
 		},
 		"Vote after end date": {
 			Mods: func(ctx weave.Context, proposal *TextProposal) {
 				blockTime, _ := weave.BlockTime(ctx)
 				proposal.VotingEndTime = weave.AsUnixTime(blockTime.Add(-1 * time.Second))
 			},
-			Msg:          VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
-			WantCheckErr: errors.ErrInvalidState,
+			Msg:            VoteMsg{ProposalId: proposalID, Selected: VoteOption_Yes, Voter: alice},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
 		},
 	}
 	auth := &weavetest.Auth{
@@ -98,18 +107,20 @@ func TestVote(t *testing.T) {
 			pBucket := withProposal(t, db, ctx, spec.Mods)
 			cache := db.CacheWrap()
 
+			// when check
 			tx := &weavetest.Tx{Msg: &spec.Msg}
 			if _, err := rt.Check(ctx, cache, tx); !spec.WantCheckErr.Is(err) {
 				t.Fatalf("check expected: %+v  but got %+v", spec.WantCheckErr, err)
 			}
 
 			cache.Discard()
-			if spec.WantCheckErr != nil {
-				// Failed checks are causing the message to be ignored.
-				return
-			}
+			// and when deliver
 			if _, err := rt.Deliver(ctx, db, tx); !spec.WantDeliverErr.Is(err) {
 				t.Fatalf("deliver expected: %+v  but got %+v", spec.WantCheckErr, err)
+			}
+
+			if spec.WantDeliverErr != nil {
+				return // skip further checks on expected error
 			}
 
 			p, err := pBucket.GetTextProposal(cache, weavetest.SequenceID(1))
@@ -125,7 +136,150 @@ func TestVote(t *testing.T) {
 
 }
 
-func withProposal(t *testing.T, db store.CacheableKVStore, ctx weave.Context, mods func(weave.Context, *TextProposal)) *ProposalBucket {
+func TestTally(t *testing.T) {
+	specs := map[string]struct {
+		Mods           func(weave.Context, *TextProposal)
+		Src            TallyResult
+		WantCheckErr   *errors.Error
+		WantDeliverErr *errors.Error
+		Exp            TextProposal_Status
+	}{
+		"Accepted with majority": {
+			Src: TallyResult{
+				TotalYes:              1,
+				TotalWeightElectorate: 1,
+				Threshold:             Fraction{Numerator: 1, Denominator: 2},
+			},
+			Exp: TextProposal_Accepted,
+		},
+		"Rejected with majority": {
+			Src: TallyResult{
+				TotalNo:               1,
+				TotalWeightElectorate: 1,
+				Threshold:             Fraction{Numerator: 1, Denominator: 2},
+			},
+			Exp: TextProposal_Rejected,
+		},
+		"Rejected by abstained": {
+			Src: TallyResult{
+				TotalAbstain:          1,
+				TotalWeightElectorate: 1,
+				Threshold:             Fraction{Numerator: 1, Denominator: 2},
+			},
+			Exp: TextProposal_Rejected,
+		},
+		"Rejected without voters": {
+			Src: TallyResult{
+				TotalWeightElectorate: 1,
+				Threshold:             Fraction{Numerator: 1, Denominator: 2},
+			},
+			Exp: TextProposal_Rejected,
+		},
+		"Rejected on threshold": {
+			Src: TallyResult{
+				TotalYes:              1,
+				TotalWeightElectorate: 2,
+				Threshold:             Fraction{Numerator: 1, Denominator: 2},
+			},
+			Exp: TextProposal_Rejected,
+		},
+		"Works with high values": {
+			Src: TallyResult{
+				TotalYes:              math.MaxUint32,
+				TotalWeightElectorate: math.MaxUint32,
+				Threshold:             Fraction{Numerator: math.MaxUint32 - 1, Denominator: math.MaxUint32},
+			},
+			Exp: TextProposal_Accepted,
+		},
+		"Fails on second tally": {
+			Mods: func(_ weave.Context, p *TextProposal) {
+				p.Status = TextProposal_Accepted
+			},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
+			Exp:            TextProposal_Accepted,
+		},
+		"Fails on tally before end date": {
+			Mods: func(ctx weave.Context, p *TextProposal) {
+				blockTime, _ := weave.BlockTime(ctx)
+				p.VotingEndTime = weave.AsUnixTime(blockTime.Add(time.Second))
+			},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
+			Exp:            TextProposal_Undefined,
+		},
+		"Fails on tally at end date": {
+			Mods: func(ctx weave.Context, p *TextProposal) {
+				blockTime, _ := weave.BlockTime(ctx)
+				p.VotingEndTime = weave.AsUnixTime(blockTime)
+			},
+			WantCheckErr:   errors.ErrInvalidState,
+			WantDeliverErr: errors.ErrInvalidState,
+			Exp:            TextProposal_Undefined,
+		},
+	}
+	auth := &weavetest.Auth{
+		Signer: aliceCond,
+	}
+	rt := app.NewRouter()
+	RegisterRoutes(rt, auth)
+
+	for msg, spec := range specs {
+		t.Run(msg, func(t *testing.T) {
+			db := store.MemStore()
+			// given
+			ctx := weave.WithBlockTime(context.Background(), time.Now().Round(time.Second))
+			setupForTally := func(_ weave.Context, p *TextProposal) {
+				p.VoteResult = spec.Src
+				blockTime, _ := weave.BlockTime(ctx)
+				p.VotingEndTime = weave.AsUnixTime(blockTime.Add(-1 * time.Second))
+			}
+			pBucket := withProposal(t, db, ctx, append([]mutator{setupForTally}, spec.Mods)...)
+			cache := db.CacheWrap()
+
+			// when check is called
+			tx := &weavetest.Tx{Msg: &TallyMsg{ProposalId: weavetest.SequenceID(1)}}
+			if _, err := rt.Check(ctx, cache, tx); !spec.WantCheckErr.Is(err) {
+				t.Fatalf("check expected: %+v  but got %+v", spec.WantCheckErr, err)
+			}
+
+			cache.Discard()
+
+			// and when deliver is called
+			res, err := rt.Deliver(ctx, db, tx)
+			if !spec.WantDeliverErr.Is(err) {
+				t.Fatalf("deliver expected: %+v  but got %+v", spec.WantCheckErr, err)
+			}
+			if spec.WantDeliverErr != nil {
+				return // skip further checks on expected error
+			}
+			// and check tags
+			exp := []common.KVPair{
+				{Key: []byte("proposal-id"), Value: weavetest.SequenceID(1)},
+				{Key: []byte("action"), Value: []byte("tally")},
+			}
+			if got := res.Tags; !reflect.DeepEqual(exp, got) {
+				t.Errorf("expected tags %v but got %v", exp, got)
+			}
+			// and check persisted status
+			p, err := pBucket.GetTextProposal(cache, weavetest.SequenceID(1))
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+			if exp, got := spec.Exp, p.Status; exp != got {
+				t.Errorf("expected %v but got %v", exp, got)
+			}
+
+			cache.Discard()
+		})
+	}
+
+}
+
+// mutator is a call back interface to modify the passed proposal for test setup
+type mutator func(weave.Context, *TextProposal)
+
+func withProposal(t *testing.T, db store.CacheableKVStore, ctx weave.Context, mods ...mutator) *ProposalBucket {
 	// setup electorate
 	electorateBucket := NewElectorateBucket()
 	err := electorateBucket.Save(db, electorateBucket.Build(db, &Electorate{
@@ -154,9 +308,12 @@ func withProposal(t *testing.T, db store.CacheableKVStore, ctx weave.Context, mo
 		ElectorateId:    weavetest.SequenceID(1),
 		VotingStartTime: weave.AsUnixTime(time.Now().Add(-1 * time.Second)),
 		VotingEndTime:   weave.AsUnixTime(time.Now().Add(time.Minute)),
+		Status:          TextProposal_Undefined,
 	}
-	if mods != nil {
-		mods(ctx, proposal)
+	for _, mod := range mods {
+		if mod != nil {
+			mod(ctx, proposal)
+		}
 	}
 	err = pBucket.Save(db, pBucket.Build(db, proposal))
 	if err != nil {
