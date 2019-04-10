805ee6f8149fb03775c5406be07ef9a86787664e
Alexander Peters <alpe@users.noreply.github.com>
2019-04-10 14:49:25 +0200

    Unify validator test to stdlib (#508)

--- x/validators/init_test.go
+++ x/validators/init_test.go
@@ -1,63 +1,57 @@
 package validators
 
 import (
-	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/store"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestInitState(t *testing.T) {
-	Convey("Test init state", t, func() {
-		// test data
-		addr := []byte("12345678901234567890")
-		addr2 := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30}
-		accts := WeaveAccounts{[]weave.Address{addr}}
+	alice := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30}
+	bert := []byte{11, 12, 13, 14, 15, 16, 17, 18, 19, 10, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30}
+	specs := map[string]struct {
+		Src      weave.Options
+		Exp      *WeaveAccounts
+		ExpError *errors.Error
+	}{
+		"Init with addresses": {
+			Src: weave.Options{optKey: []byte(`{"addresses":["0102030405060708090021222324252627282930", "0B0C0D0E0F101112130A21222324252627282930"]}`)},
+			Exp: &WeaveAccounts{[]weave.Address{alice, bert}},
+		},
+		"Init works with no appState data": {
+			Src: weave.Options{},
+		},
+		"Init works with no relevant appState data": {
+			Src: weave.Options{"foo": []byte(`"bar"`)},
+		},
+		"Init fails with bad address": {
+			Src:      weave.Options{optKey: []byte(`{"addresses":["00"]}`)},
+			ExpError: errors.ErrInvalidInput,
+		},
+	}
 
-		accountsJson, err := json.Marshal(accts)
-		So(err, ShouldBeNil)
-
-		accountsJson2 := []byte(`{"addresses":["0102030405060708090021222324252627282930"]}`)
-		accts2 := WeaveAccounts{[]weave.Address{addr2}}
-
-		init := Initializer{}
-
-		kv := store.MemStore()
-		bucket := NewBucket()
-
-		Convey("Init works with no appState data", func() {
-			err := init.FromGenesis(weave.Options{}, kv)
-			So(err, ShouldBeNil)
-		})
-
-		Convey("Init works with no relevant appState data", func() {
-			err := init.FromGenesis(weave.Options{"foo": []byte(`"bar"`)}, kv)
-			So(err, ShouldBeNil)
-		})
-
-		Convey("Init fails with bad address", func() {
-			err := init.FromGenesis(weave.Options{optKey: []byte(`{"addresses":["as"]}`)}, kv)
-			So(err, ShouldNotBeNil)
-		})
-
-		Convey("Init succeeds with marshalled contents", func() {
-			err := init.FromGenesis(weave.Options{optKey: accountsJson}, kv)
-			So(err, ShouldBeNil)
-
+	for msg, spec := range specs {
+		t.Run(msg, func(t *testing.T) {
+			kv := store.MemStore()
+			bucket := NewBucket()
+			// when
+			err := Initializer{}.FromGenesis(spec.Src, kv)
+			if !spec.ExpError.Is(err) {
+				t.Fatalf("check expected: %v  but got %+v", spec.ExpError, err)
+			}
+			if spec.Exp == nil {
+				return
+			}
 			accounts, err := GetAccounts(bucket, kv)
-			So(err, ShouldBeNil)
-			So(accounts, ShouldResemble, AsAccounts(accts))
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+			if exp, got := AsAccounts(*spec.Exp), accounts; !reflect.DeepEqual(exp, got) {
+				t.Errorf("expected %v but got %v", exp, got)
+			}
 		})
-
-		Convey("Init succeeds with hardcoded contents", func() {
-			err := init.FromGenesis(weave.Options{optKey: accountsJson2}, kv)
-			So(err, ShouldBeNil)
-
-			accounts, err := GetAccounts(bucket, kv)
-			So(err, ShouldBeNil)
-			So(accounts, ShouldResemble, AsAccounts(accts2))
-		})
-	})
+	}
 }
