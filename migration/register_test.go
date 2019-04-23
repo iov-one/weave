3078b876bbfa4d1939d650c2a602315a4e29c575
Piotr Husiatyński <phusiatynski@gmail.com>
2019-04-23 20:16:45 +0200

    migrations: pull request review

--- migration/register_test.go
+++ migration/register_test.go
@@ -8,45 +8,75 @@ import (
 	"github.com/iov-one/weave/weavetest/assert"
 )
 
+func TestZeroMigrationIsNotAllowed(t *testing.T) {
+	reg := newRegister()
+
+	if err := reg.Register(0, &MyMsg{}, NoModification); !errors.ErrInvalidInput.Is(err) {
+		t.Fatalf("unexpected invalid version registration error: %s", err)
+	}
+	if err := reg.Apply(nil, nil, &MyMsg{}, 0); !errors.ErrInvalidInput.Is(err) {
+		t.Fatalf("unexpected invalid version registration error: %s", err)
+	}
+}
+
+func TestRegisterMigrationMustBeSequential(t *testing.T) {
+	reg := newRegister()
+
+	// Each migration must start with 1.
+	if err := reg.Register(2, &MyMsg{}, NoModification); !errors.ErrInvalidInput.Is(err) {
+		t.Fatalf("unexpected error when missing previous migration: %s", err)
+	}
+
+	reg.MustRegister(1, &MyMsg{}, NoModification)
+	reg.MustRegister(2, &MyMsg{}, NoModification)
+
+	if err := reg.Register(4, &MyMsg{}, NoModification); !errors.ErrInvalidInput.Is(err) {
+		t.Fatalf("unexpected error when missing previous migration: %s", err)
+	}
+
+	reg.MustRegister(3, &MyMsg{}, NoModification)
+	reg.MustRegister(4, &MyMsg{}, NoModification)
+}
+
 func TestApply(t *testing.T) {
 	reg := newRegister()
-	reg.MustRegister(0, &MyMsg{}, NoModyfication)
-	reg.MustRegister(1, &MyMsg{}, func(ctx weave.Context, db weave.KVStore, p Payload) error {
+	reg.MustRegister(1, &MyMsg{}, NoModification)
+	reg.MustRegister(2, &MyMsg{}, func(ctx weave.Context, db weave.KVStore, p Payload) error {
 		msg := p.(*MyMsg)
-		msg.Content += "to1"
+		msg.Content += "to2"
 		return nil
 	})
-	reg.MustRegister(2, &MyMsg{}, NoModyfication)
-	reg.MustRegister(3, &MyMsg{}, func(ctx weave.Context, db weave.KVStore, p Payload) error {
+	reg.MustRegister(3, &MyMsg{}, NoModification)
+	reg.MustRegister(4, &MyMsg{}, func(ctx weave.Context, db weave.KVStore, p Payload) error {
 		msg := p.(*MyMsg)
-		msg.Content += "to3"
+		msg.Content += "to4"
 		return nil
 	})
 
 	mymsg := &MyMsg{
-		Header:  &weave.Header{},
-		Content: "zero ",
+		Header:  &weave.Header{Schema: 1},
+		Content: "init ",
 	}
 
 	// Running a migration can bring it up to any state in the future.
-	assert.Nil(t, reg.Apply(nil, nil, mymsg, 2))
-	assert.Equal(t, mymsg.Header.Schema, uint32(2))
-	assert.Equal(t, mymsg.Content, "zero to1")
-
 	assert.Nil(t, reg.Apply(nil, nil, mymsg, 3))
 	assert.Equal(t, mymsg.Header.Schema, uint32(3))
-	assert.Equal(t, mymsg.Content, "zero to1to3")
+	assert.Equal(t, mymsg.Content, "init to2")
+
+	assert.Nil(t, reg.Apply(nil, nil, mymsg, 4))
+	assert.Equal(t, mymsg.Header.Schema, uint32(4))
+	assert.Equal(t, mymsg.Content, "init to2to4")
 }
 
 func TestMigrateUnknownVersion(t *testing.T) {
 	reg := newRegister()
-	reg.MustRegister(0, &MyMsg{}, NoModyfication)
-	reg.MustRegister(1, &MyMsg{}, NoModyfication)
-	reg.MustRegister(2, &MyMsg{}, NoModyfication)
+	reg.MustRegister(1, &MyMsg{}, NoModification)
+	reg.MustRegister(2, &MyMsg{}, NoModification)
+	reg.MustRegister(3, &MyMsg{}, NoModification)
 
 	mymsg := &MyMsg{
-		Header:  &weave.Header{},
-		Content: "zero ",
+		Header:  &weave.Header{Schema: 1},
+		Content: "init ",
 	}
 
 	// Migration attempt to a non existing version must fail. It will
@@ -54,7 +84,7 @@ func TestMigrateUnknownVersion(t *testing.T) {
 	if err := reg.Apply(nil, nil, mymsg, 999); !errors.ErrInvalidState.Is(err) {
 		t.Fatalf("unexpected migration failure: %s", err)
 	}
-	assert.Equal(t, mymsg.Header.Schema, uint32(2))
+	assert.Equal(t, mymsg.Header.Schema, uint32(3))
 }
 
 type MyMsg struct {
