910a2f96aabfb81c820e0cfdf4b15290f84c3731
Piotr Husiatyński <phusiatynski@gmail.com>
2019-04-23 20:23:58 +0200

    migrations: rename header to metadata

--- migration/codec.pb.go
+++ migration/codec.pb.go
@@ -24,7 +24,7 @@ const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
 // Schema declares the maxiumum supported schema version for a package.
 type Schema struct {
-	Header *weave.Header `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	Metadata *weave.Metadata `protobuf:"bytes,1,opt,name=metadata,proto3" json:"metadata,omitempty"`
 	// Pkg holds the name of the package that this migration is declared for.
 	// For example, for extension `x/myext` package value is `myext`
 	Pkg string `protobuf:"bytes,2,opt,name=pkg,proto3" json:"pkg,omitempty"`
@@ -65,9 +65,9 @@ func (m *Schema) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Schema proto.InternalMessageInfo
 
-func (m *Schema) GetHeader() *weave.Header {
+func (m *Schema) GetMetadata() *weave.Metadata {
 	if m != nil {
-		return m.Header
+		return m.Metadata
 	}
 	return nil
 }
@@ -89,7 +89,7 @@ func (m *Schema) GetVersion() uint32 {
 // UpgradeSchemaMsg is a request to upgrade schema version of a given package
 // by one version.
 type UpgradeSchemaMsg struct {
-	Header *weave.Header `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	Metadata *weave.Metadata `protobuf:"bytes,1,opt,name=metadata,proto3" json:"metadata,omitempty"`
 	// Name of the package that schema version upgrade is made for.
 	Pkg string `protobuf:"bytes,2,opt,name=pkg,proto3" json:"pkg,omitempty"`
 }
@@ -127,9 +127,9 @@ func (m *UpgradeSchemaMsg) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UpgradeSchemaMsg proto.InternalMessageInfo
 
-func (m *UpgradeSchemaMsg) GetHeader() *weave.Header {
+func (m *UpgradeSchemaMsg) GetMetadata() *weave.Metadata {
 	if m != nil {
-		return m.Header
+		return m.Metadata
 	}
 	return nil
 }
@@ -149,20 +149,20 @@ func init() {
 func init() { proto.RegisterFile("migration/codec.proto", fileDescriptor_ecf669b5eede564b) }
 
 var fileDescriptor_ecf669b5eede564b = []byte{
-	// 204 bytes of a gzipped FileDescriptorProto
+	// 206 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcd, 0xcd, 0x4c, 0x2f,
 	0x4a, 0x2c, 0xc9, 0xcc, 0xcf, 0xd3, 0x4f, 0xce, 0x4f, 0x49, 0x4d, 0xd6, 0x2b, 0x28, 0xca, 0x2f,
 	0xc9, 0x17, 0xe2, 0x84, 0x0b, 0x4b, 0xa9, 0xa4, 0x67, 0x96, 0x64, 0x94, 0x26, 0xe9, 0x25, 0xe7,
 	0xe7, 0xea, 0x67, 0xe6, 0x97, 0xe9, 0xe6, 0xe7, 0xa5, 0xea, 0x97, 0xa7, 0x26, 0x96, 0xa5, 0x22,
-	0x6b, 0x50, 0x8a, 0xe6, 0x62, 0x0b, 0x4e, 0xce, 0x48, 0xcd, 0x4d, 0x14, 0x52, 0xe5, 0x62, 0xcb,
-	0x48, 0x4d, 0x4c, 0x49, 0x2d, 0x92, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x36, 0xe2, 0xd5, 0x03, 0xab,
-	0xd6, 0xf3, 0x00, 0x0b, 0x06, 0x41, 0x25, 0x85, 0x04, 0xb8, 0x98, 0x0b, 0xb2, 0xd3, 0x25, 0x98,
-	0x14, 0x18, 0x35, 0x38, 0x83, 0x40, 0x4c, 0x21, 0x09, 0x2e, 0xf6, 0xb2, 0xd4, 0xa2, 0xe2, 0xcc,
-	0xfc, 0x3c, 0x09, 0x66, 0x05, 0x46, 0x0d, 0xde, 0x20, 0x18, 0x57, 0xc9, 0x9b, 0x4b, 0x20, 0xb4,
-	0x20, 0xbd, 0x28, 0x31, 0x25, 0x15, 0x62, 0x87, 0x6f, 0x71, 0x3a, 0xd9, 0xd6, 0x38, 0x49, 0x9c,
-	0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31,
-	0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x12, 0x1b, 0xd8, 0x2b, 0xc6, 0x80, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0x70, 0x8f, 0x25, 0x75, 0x14, 0x01, 0x00, 0x00,
+	0x6b, 0x50, 0x4a, 0xe4, 0x62, 0x0b, 0x4e, 0xce, 0x48, 0xcd, 0x4d, 0x14, 0xd2, 0xe6, 0xe2, 0xc8,
+	0x4d, 0x2d, 0x49, 0x4c, 0x49, 0x2c, 0x49, 0x94, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x36, 0xe2, 0xd7,
+	0x03, 0xab, 0xd7, 0xf3, 0x85, 0x0a, 0x07, 0xc1, 0x15, 0x08, 0x09, 0x70, 0x31, 0x17, 0x64, 0xa7,
+	0x4b, 0x30, 0x29, 0x30, 0x6a, 0x70, 0x06, 0x81, 0x98, 0x42, 0x12, 0x5c, 0xec, 0x65, 0xa9, 0x45,
+	0xc5, 0x99, 0xf9, 0x79, 0x12, 0xcc, 0x0a, 0x8c, 0x1a, 0xbc, 0x41, 0x30, 0xae, 0x52, 0x20, 0x97,
+	0x40, 0x68, 0x41, 0x7a, 0x51, 0x62, 0x4a, 0x2a, 0xc4, 0x26, 0xdf, 0xe2, 0x74, 0x0a, 0x2d, 0x73,
+	0x92, 0x38, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c,
+	0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6, 0x63, 0x39, 0x86, 0x24, 0x36, 0xb0, 0xb7, 0x8c,
+	0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x86, 0xbb, 0x13, 0x55, 0x20, 0x01, 0x00, 0x00,
 }
 
 func (m *Schema) Marshal() (dAtA []byte, err error) {
@@ -180,11 +180,11 @@ func (m *Schema) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if m.Header != nil {
+	if m.Metadata != nil {
 		dAtA[i] = 0xa
 		i++
-		i = encodeVarintCodec(dAtA, i, uint64(m.Header.Size()))
-		n1, err := m.Header.MarshalTo(dAtA[i:])
+		i = encodeVarintCodec(dAtA, i, uint64(m.Metadata.Size()))
+		n1, err := m.Metadata.MarshalTo(dAtA[i:])
 		if err != nil {
 			return 0, err
 		}
@@ -219,11 +219,11 @@ func (m *UpgradeSchemaMsg) MarshalTo(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if m.Header != nil {
+	if m.Metadata != nil {
 		dAtA[i] = 0xa
 		i++
-		i = encodeVarintCodec(dAtA, i, uint64(m.Header.Size()))
-		n2, err := m.Header.MarshalTo(dAtA[i:])
+		i = encodeVarintCodec(dAtA, i, uint64(m.Metadata.Size()))
+		n2, err := m.Metadata.MarshalTo(dAtA[i:])
 		if err != nil {
 			return 0, err
 		}
@@ -253,8 +253,8 @@ func (m *Schema) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.Header != nil {
-		l = m.Header.Size()
+	if m.Metadata != nil {
+		l = m.Metadata.Size()
 		n += 1 + l + sovCodec(uint64(l))
 	}
 	l = len(m.Pkg)
@@ -273,8 +273,8 @@ func (m *UpgradeSchemaMsg) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.Header != nil {
-		l = m.Header.Size()
+	if m.Metadata != nil {
+		l = m.Metadata.Size()
 		n += 1 + l + sovCodec(uint64(l))
 	}
 	l = len(m.Pkg)
@@ -328,7 +328,7 @@ func (m *Schema) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Header", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Metadata", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -355,10 +355,10 @@ func (m *Schema) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if m.Header == nil {
-				m.Header = &weave.Header{}
+			if m.Metadata == nil {
+				m.Metadata = &weave.Metadata{}
 			}
-			if err := m.Header.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if err := m.Metadata.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -468,7 +468,7 @@ func (m *UpgradeSchemaMsg) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Header", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Metadata", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -495,10 +495,10 @@ func (m *UpgradeSchemaMsg) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if m.Header == nil {
-				m.Header = &weave.Header{}
+			if m.Metadata == nil {
+				m.Metadata = &weave.Metadata{}
 			}
-			if err := m.Header.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if err := m.Metadata.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
