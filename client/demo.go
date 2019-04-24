9bb8f68b0d908b301dc1a8d473b9b166c8852940
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-24 03:26:08 +0200

    Combine subscribe and search into watch

--- client/demo.go
+++ client/demo.go
@@ -18,28 +18,39 @@ type AsyncResult struct {
 	Err    error
 }
 
+func (a AsyncResult) AsSyncError() SyncResult {
+	return SyncResult{
+		ID:  a.ID,
+		Err: a.Err,
+	}
+}
+
 type SyncResult struct {
 	ID     TransactionId
+	Height int64
 	Result *weave.DeliverResult
 	Err    error
 }
 
-// ??? needed ???
-type CommitResult struct {
-	ID      TransactionId
-	Check   *weave.CheckResult
-	Deliver *weave.DeliverResult
-	Err     error
-}
-
-// SubmitTx will block on both Check and Deliver, returning when it is in a block
-func (c *Client) SubmitTx(ctx context.Context, tx weave.Tx) SyncResult {
-	return SyncResult{}
-}
-
-// This just blocks on Check, then can
+// This just blocks on Check, then returns result or error.
+// ID will always be set
 func (c *Client) SubmitTxAsync(ctx context.Context, tx weave.Tx) AsyncResult {
+	// TODO: submit to the node
 	return AsyncResult{}
+}
+
+// SearchTxById will return 0 or 1 results (nil or result value)
+func (c *Client) SearchTxById(ctx context.Context, id TransactionId) *SyncResult {
+	// TODO: search
+	return nil
+}
+
+// SubscribeTxById will block until there is a result, then return it
+// You must cancel the context to avoid blocking forever in some cases
+func (c *Client) SubscribeTxById(ctx context.Context, id TransactionId) SyncResult {
+	// TODO: subscribe
+	// TODO: how to handle context being cancelled???
+	return SyncResult{}
 }
 
 // WatchTx will block until this transaction makes it into a block
@@ -47,7 +58,35 @@ func (c *Client) SubmitTxAsync(ctx context.Context, tx weave.Tx) AsyncResult {
 // You can use context.Context to pass in a timeout
 func (c *Client) WatchTx(ctx context.Context, id TransactionId) SyncResult {
 	// TODO: combine subscribe tx and search tx (two other functions to be writen)
-	return SyncResult{}
+	subctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// start a subscription
+	sub := make(chan SyncResult, 1)
+	go func() {
+		res := c.SubscribeTxById(subctx, id)
+		sub <- res
+	}()
+
+	// try to search and if successful, abort the subscription
+	search := c.SearchTxById(ctx, id)
+	if search != nil {
+		return *search
+	}
+
+	// now we just wait until the subscription returns fruit
+	result := <-sub
+	return result
+}
+
+// SubmitTx will block on both Check and Deliver, returning when it is in a block
+func (c *Client) SubmitTx(ctx context.Context, tx weave.Tx) SyncResult {
+	// This can be combined from other primatives
+	check := c.SubmitTxAsync(ctx, tx)
+	if check.Err != nil {
+		return check.AsSyncError()
+	}
+	return c.WatchTx(ctx, check.ID)
 }
 
 // WatchTxs will watch a list of transactions in parallel
@@ -90,8 +129,7 @@ func (c *Client) SubmitTxs(ctx context.Context, txs []weave.Tx) []SyncResult {
 	// now, we combine the check errors into the deliver results
 	for i, check := range checks {
 		if check.Err != nil {
-			results[i].ID = check.ID
-			results[i].Err = check.Err
+			results[i] = check.AsSyncError()
 		}
 	}
 
