c596577c73613ce10e5347ee586e9e64e0d30ad4
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-25 19:26:13 +0200

    Remove MempoolResult in favor of just ID

--- client/types.go
+++ client/types.go
@@ -21,24 +21,6 @@ type ResponseQuery = abci.ResponseQuery
 // TxQuery is some query to find transactions
 type TxQuery = string
 
-// MempoolResult is returned from the mempool (CheckTx)
-// Result is only set on success codes, Err is set if it was a failure code
-type MempoolResult struct {
-	ID  TransactionID
-	Err error
-}
-
-// AsCommitError will turn an errored MempoolResult into a CommitResult
-func (a *MempoolResult) AsCommitError() *CommitResult {
-	if a.Err == nil {
-		panic("failed assertion: AsCommitError can onyl be called on errors")
-	}
-	return &CommitResult{
-		ID:  a.ID,
-		Err: a.Err,
-	}
-}
-
 // CommitResult is returned from the block (DeliverTx)
 // Result is only set on success codes, Err is set if it was a failure code
 type CommitResult struct {
@@ -55,9 +37,10 @@ type Status struct {
 	CatchingUp bool
 }
 
+// Header is a tendermint block header
 type Header = tmtypes.Header
 
-// type Status = ctypes.ResultStatus
+// GenesisDoc is the full tendermint genesis file
 type GenesisDoc = tmtypes.GenesisDoc
 
 type resultOrError struct {
