bf3e98bdf0a3207842c724c02c43fa730b7ecd59
Roman Useinov <roman.useinov@gmail.com>
2019-05-15 13:04:43 +0200

    errors: multiErr api changes and usage examples

--- x/batch/msg.go
+++ x/batch/msg.go
@@ -16,13 +16,17 @@ type Msg interface {
 
 func Validate(msg Msg) error {
 	l, err := msg.MsgList()
-	if err != nil {
-		return errors.Wrap(err, "cannot retrieve batch message")
-	}
+	multiErr := errors.MultiAddNamed("Message", errors.Wrap(err, "cannot retrieve batch message"))
 
 	msgNum := len(l)
 	if msgNum > MaxBatchMessages {
-		return errors.Wrapf(errors.ErrInput, "transaction is too large, max: %d vs current: %d", MaxBatchMessages, msgNum)
+		multiErr.AddNamed("Size", errors.Wrapf(errors.ErrInput,
+			"transaction is too large, max: %d vs current: %d", MaxBatchMessages, msgNum))
 	}
-	return nil
+
+	if multiErr.IsEmpty() {
+		return nil
+	}
+
+	return multiErr
 }
