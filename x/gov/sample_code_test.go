07af9a1fc4f2b27a4a0d87e204a9fd4a019a5256
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-05-20 09:20:56 +0200

    Tested and demoed using App* messages for one-step parsing

--- x/gov/sample_code_test.go
+++ x/gov/sample_code_test.go
@@ -16,6 +16,8 @@ func decodeProposalOptions(raw []byte) (weave.Msg, error) {
 	return weave.ExtractMsgFromSum(model.Option)
 }
 
+// proposalOptionsExecutor will set up an executor to allow governance-internal actions
+// such a setup can be easily extended to allow many more actions in other modules
 func proposalOptionsExecutor() Executor {
 	r := app.NewRouter()
 	// we only allow these to be authenticated by the governance context, not by sigs or other items
