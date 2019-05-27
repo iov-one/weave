880cf4f3f3ee9eb3bf5e8048efe6723c0100499a
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-05-27 12:57:42 +0200

    Update scripts per PR comments

--- scripts/build_protodocs_docker.sh
+++ scripts/build_protodocs_docker.sh
@@ -5,9 +5,9 @@ set -o errexit -o nounset -o pipefail
 # This is a version of build_protodocs, which uses docker images to build, and includes gogoproto support,
 # so it doesn't need the pre-clean step
 
-USER=$(id -u):$(id -g)
-PROTOC="docker run --rm --user ${USER} --mount type=bind,source=$(pwd),target=/work --tmpfs /tmp:exec iov1/prototool-docker protoc"
-PROTOTOOL="docker run --rm --user ${USER} --mount type=bind,source=$(pwd),target=/work --tmpfs /tmp:exec iov1/prototool-docker prototool"
+user=$(id -u):$(id -g)
+protoc="docker run --rm --user ${user} --mount type=bind,source=$(pwd),target=/work --tmpfs /tmp:exec iov1/prototool-docker protoc"
+prototool="docker run --rm --user ${user} --mount type=bind,source=$(pwd),target=/work --tmpfs /tmp:exec iov1/prototool-docker prototool"
 
-FILES=$(${PROTOTOOL} files | grep -v examples | grep -v cmd/bcpd | sort)
-${PROTOC} -I . -I /usr/include --doc_out=docs/proto --doc_opt=html,index.html ${FILES}
+files=$(${prototool} files | grep -v examples | grep -v cmd/bcpd | sort)
+${protoc} -I . -I /usr/include --doc_out=docs/proto --doc_opt=html,index.html ${files}
