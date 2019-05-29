048862d901406cda79cb77d10e0c2b718e53921f
Theofanis Despoudis <thdespou@hotmail.com>
2019-05-29 13:58:08 +0100

    Update index_test.go

--- orm/index_test.go
+++ orm/index_test.go
@@ -164,7 +164,7 @@ func TestCounterMultiKeyIndex(t *testing.T) {
 			expKeys:  [][]byte{encodeSequence(6), []byte("even")},
 			expError: false,
 		},
-		{ // id missmatch
+		{ // id mismatch
 			index:    uniq,
 			prev:     NewSimpleObj([]byte("my"), NewCounter(5)),
 			next:     NewSimpleObj([]byte("bar"), NewCounter(7)),
