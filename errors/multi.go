37ce2b6ee393823bcda876c43e948d257862476d
Alexander Peters <alpe@users.noreply.github.com>
2019-05-31 15:35:47 +0200

    Puristic multiErr (#693)
    * errors: initial mutierr
    
    * errors: implement is helper for multiErr
    
    * errors: simplify multiErr implementation
    
    * errors: polish multiErr Error() method
    
    * errors: add helpers to create multiErr
    
    * errors: more multierr comments
    
    * errors: multiErr implementation clarifications
    
    * errors: add multiErr tests
    
    * errors: add multiErr constructor
    
    * errors: multiErr api changes and usage examples
    
    * errors: implement AsMulti helper
    
    * errors: enable nested wrapping
    
    * errors: return multiErr on multiple named errors for one field
    
    * errors: make linter happy
    
    * Puristic multiErr
    
    * Introduce Append method for new multiErr
    
    * Revert changes in x/batch
    
    * Fix typos in test method names
    
    * Add multiErr to changelog

---
+++ errors/multi.go
@@ -0,0 +1,98 @@
+package errors
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Append combines two errors. Any value can be nil.
+// When both are nil then nil is returned, too.
+// When only one is not nil, that one is returned.
+// Otherwise the result is of type multiErr.
+func Append(src, new error) error {
+	switch {
+	case new == nil:
+		return src
+	case src == nil:
+		return new
+	}
+	return multiErr{}.with(src).with(new)
+}
+
+const multiErrCode uint32 = 1000
+
+var _ coder = (*multiErr)(nil)
+
+type multiErr []error
+
+// IsEmpty returns true if there are no errors registered,
+func (e multiErr) IsEmpty() bool {
+	return len(e) == 0
+}
+
+// with returns a new multiErr instance with the given error added.
+// Nil values are ignored and multiErr flattened.
+func (e multiErr) with(source error) multiErr {
+	switch err := source.(type) {
+	case nil:
+		return e
+	case multiErr:
+		return e.append(err...)
+	case *wrappedError:
+		root, msgs := unWrap(err)
+
+		me, ok := root.(multiErr)
+		if !ok {
+			return e.append(source)
+		}
+		// flatten and re-wrap errors
+		result := e
+		for _, v := range me {
+			rErr := v
+			for _, m := range msgs {
+				rErr = Wrap(rErr, m)
+			}
+			result = append(result, rErr)
+		}
+		return result
+	}
+	return e.append(source)
+}
+
+// append copies values into a new array to not let stdlib append modify the the original one
+func (e multiErr) append(errs ...error) multiErr {
+	r := make(multiErr, len(e), len(e)+len(errs))
+	copy(r, e)
+	return append(r, errs...)
+}
+
+// Error satisfies the error interface and returns a serialized version of the content.
+func (e multiErr) Error() string {
+	if e.IsEmpty() {
+		return ""
+	}
+
+	errs := make([]string, len(e))
+	for i, err := range e {
+		errs[i] = fmt.Sprintf("* %s", err)
+	}
+
+	return fmt.Sprintf(
+		"%d errors occurred:\n\t%s\n\n",
+		len(e), strings.Join(errs, "\n\t"))
+}
+
+// ABCICode returns 1000
+func (e multiErr) ABCICode() uint32 {
+	return multiErrCode
+}
+
+// Contains returns true when the given error instance is element of this multiErr.
+func (e multiErr) Contains(err *Error) bool {
+	for _, v := range e {
+		if err.Is(v) {
+			return true
+		}
+	}
+	return false
+}
---
+++ errors/multi_test.go
@@ -0,0 +1,92 @@
+package errors
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/iov-one/weave/weavetest/assert"
+	"github.com/pkg/errors"
+)
+
+func TestAddToMultiErr(t *testing.T) {
+	var (
+		// create errors with stacktrace for equal comparision
+		myErrNotFound = errors.WithStack(ErrNotFound)
+		myErrState    = errors.WithStack(ErrState)
+		myErrMsg      = errors.WithStack(ErrMsg)
+	)
+	specs := map[string]struct {
+		src error
+		add error
+		exp error
+	}{
+		"Append with first nil":    {src: nil, add: myErrNotFound, exp: myErrNotFound},
+		"Append with second nil":   {src: myErrNotFound, add: nil, exp: myErrNotFound},
+		"Append with both nil":     {src: nil, add: nil, exp: nil},
+		"Append with both not nil": {src: myErrNotFound, add: myErrMsg, exp: multiErr{myErrNotFound, myErrMsg}},
+		"Append multiErr should be flattened": {
+			src: myErrNotFound, add: Append(myErrState, myErrMsg), exp: multiErr{myErrNotFound, myErrState, myErrMsg},
+		},
+		"Append first wrapped multiErr should be flattened": {
+			src: Wrap(Append(myErrState, myErrMsg), "test"),
+			add: ErrHuman,
+			exp: multiErr{Wrap(myErrState, "test"), Wrap(myErrMsg, "test"), ErrHuman},
+		},
+		"Append second wrapped multiErr should be flattened": {
+			src: myErrNotFound,
+			add: Wrap(Append(myErrState, myErrMsg), "test"),
+			exp: multiErr{myErrNotFound, Wrap(myErrState, "test"), Wrap(myErrMsg, "test")},
+		},
+		"Append double wrapped multiErr should be flattened": {
+			src: myErrNotFound,
+			add: Wrap(Wrap(Append(myErrState, myErrMsg), "first"), "second"),
+			exp: multiErr{
+				myErrNotFound,
+				Wrap(Wrap(myErrState, "first"), "second"),
+				Wrap(Wrap(myErrMsg, "first"), "second"),
+			},
+		},
+	}
+	for msg, spec := range specs {
+		t.Run(msg, func(t *testing.T) {
+			mErr := Append(spec.src, spec.add)
+			// then
+			if exp, got := spec.exp, mErr; !reflect.DeepEqual(exp, got) {
+				t.Errorf("expected %#v but got %#v", exp, got)
+			}
+		})
+	}
+}
+
+func TestMultiErrIsEmpty(t *testing.T) {
+	specs := map[string]struct {
+		src multiErr
+		exp bool
+	}{
+		"Single error": {src: multiErr{ErrNotFound}, exp: false},
+		"Empty":        {src: multiErr{}, exp: true},
+		"nil":          {src: nil, exp: true},
+	}
+	for msg, spec := range specs {
+		t.Run(msg, func(t *testing.T) {
+			// then
+			if exp, got := spec.exp, spec.src.IsEmpty(); !reflect.DeepEqual(exp, got) {
+				t.Errorf("expected %v but got %v", exp, got)
+			}
+
+		})
+	}
+}
+
+func TestMultiErrABCICode(t *testing.T) {
+	var mErr multiErr
+	if exp, got := uint32(1000), mErr.ABCICode(); exp != got {
+		t.Errorf("expected %v but got %v", exp, got)
+	}
+}
+
+func TestMultiErrABCICodeRegistered(t *testing.T) {
+	assert.Panics(t, func() {
+		Register(multiErrCode, "fails")
+	})
+}
