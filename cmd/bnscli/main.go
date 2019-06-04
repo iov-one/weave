61c7278cfdf7f8b618e4490d4b3cbea4eb7762bb
Piotr Husiatyński <phusiatynski@gmail.com>
2019-06-04 11:13:04 +0200

    cmd/bnscli: improve and add Makefile

--- cmd/bnscli/main.go
+++ cmd/bnscli/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -35,28 +36,21 @@ var commands = map[string]func(input io.Reader, output io.Writer, args []string)
 	"escrow-proposal":   cmdNewEscrowProposal,
 	"sign":              cmdSignTransaction,
 	"submit":            cmdSubmitTx,
+	"version":           cmdVersion,
 }
 
 func main() {
 	if len(os.Args) == 1 {
-		available := make([]string, 0, len(commands))
-		for name := range commands {
-			available = append(available, name)
-		}
 		fmt.Fprintf(os.Stderr, "%s is a command line client for the BNSD application.\n\n", os.Args[0])
 		fmt.Fprintf(os.Stderr, "Usage: %s <command> [<flags>]\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "\nAvailable commands are:\n\t%s\n", strings.Join(available, "\n\t"))
-		fmt.Fprintf(os.Stderr, "Use <command> -help to learn more about each command.\n")
+		fmt.Fprintf(os.Stderr, "\nAvailable commands are:\n\t%s\n", strings.Join(availableCmds(), "\n\t"))
+		fmt.Fprintf(os.Stderr, "Run '%s <command> -help' to learn more about each command.\n", os.Args[0])
 		os.Exit(2)
 	}
 	run, ok := commands[os.Args[1]]
 	if !ok {
-		available := make([]string, 0, len(commands))
-		for name := range commands {
-			available = append(available, name)
-		}
 		fmt.Fprintf(os.Stderr, "Unknown command %q\n", os.Args[1])
-		fmt.Fprintf(os.Stderr, "\nAvailable commands are:\n\t%s\n", strings.Join(available, "\n\t"))
+		fmt.Fprintf(os.Stderr, "\nAvailable commands are:\n\t%s\n", strings.Join(availableCmds(), "\n\t"))
 		os.Exit(2)
 	}
 
@@ -67,3 +61,20 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+func availableCmds() []string {
+	available := make([]string, 0, len(commands))
+	for name := range commands {
+		available = append(available, name)
+	}
+	sort.Strings(available)
+	return available
+}
+
+func cmdVersion(in io.Reader, out io.Writer, args []string) error {
+	fmt.Fprintln(out, gitHash)
+	return nil
+}
+
+// gitHash is set during the compilation time.
+var gitHash string = "dev"
