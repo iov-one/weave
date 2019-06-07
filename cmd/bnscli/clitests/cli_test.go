f29aec9ddd5603a635c364c01a7f525790329e49
Piotr Husiatyński <phusiatynski@gmail.com>
2019-06-07 09:19:35 +0200

    cmd/bnscli: improve tests
    Add tests for `sign` and `submit` command. This requires a tendermint
    server to be reachable. For tests to have reproducible result a mock
    tendermint server is started an TM_ADDR is enforced.
    
    Remove temporary file at the end of each script.

--- cmd/bnscli/clitests/cli_test.go
+++ cmd/bnscli/clitests/cli_test.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bytes"
 	"flag"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
@@ -19,11 +23,32 @@ func TestAll(t *testing.T) {
 	if len(testFiles) == 0 {
 		t.Skip("no test files found")
 	}
+
+	tm := fakeTendermintServer(t)
+	defer tm.Close()
+
 	for _, tf := range testFiles {
 		t.Run(tf, func(t *testing.T) {
 			cmd := exec.Command("/bin/sh", tf)
+
+			// Use host's environment to run the tests. This allows
+			// to provide the same setup as when running each
+			// script directly.
+			// To ensure that all commands are using tendermint
+			// mock server, set environment variable to enforce
+			// that.
+			cmd.Env = append(os.Environ(), "TM_ADDR="+tm.URL)
+
+			stderr, err := cmd.StderrPipe()
+			if err != nil {
+				t.Fatalf("cannot get commands stderr: %s", err)
+			}
+
 			out, err := cmd.Output()
 			if err != nil {
+				if b, _ := ioutil.ReadAll(stderr); len(b) != 0 {
+					t.Logf("\nSTDERR\n%s\n\n", string(b))
+				}
 				t.Fatalf("execution failed: %s", err)
 			}
 
@@ -47,4 +72,44 @@ func TestAll(t *testing.T) {
 			}
 		})
 	}
+}
+
+// fakeTendermintServer creates an HTTP server that acts similar to the
+// tendermint HTTP server. It does not maintain any state or return fully
+// correct responses but its behaviour is good enough to fool bnscli.
+func fakeTendermintServer(t *testing.T) *httptest.Server {
+	t.Helper()
+
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.Method == "GET" && r.URL.Path == "/genesis":
+			io.WriteString(w, `
+				{
+					"jsonrpc": "2.0",
+					"id": "",
+					"result": {
+						"genesis": {
+							"chain_id": "bnscli-test-fake-chain",
+							"validators": [],
+							"app_state": {}
+						}
+					}
+				}
+			`)
+		case r.Method == "POST" && r.URL.Path == "/":
+			// This is an RPC call - response always depends on the
+			// submitted content. For our tests it does not matter
+			// that much what is returned.
+			io.WriteString(w, `
+				{
+					"jsonrpc": "2.0",
+					"id": "jsonrpc-client",
+					"result": {"response": {"height": "12345"}}
+				}
+			`)
+
+		default:
+			http.Error(w, "not implemented", http.StatusNotImplemented)
+		}
+	}))
 }
