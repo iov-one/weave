0e2c1d15128641c678043a5a6a1a7c35fdab1888
Piotr Husiatyński <phusiatynski@gmail.com>
2019-06-11 15:43:37 +0200

    cmd/bnscli: a new set of multisig commands
    A new set of commands to manage multisig extension functionality.
    Create/update contract and attach multisig to a transaction header.
    
    resolve #741

---
+++ cmd/bnscli/cmd_multisig.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+
+	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/cmd/bnsd/app"
+	"github.com/iov-one/weave/x/multisig"
+)
+
+func cmdMultisig(input io.Reader, output io.Writer, args []string) error {
+	fl := flag.NewFlagSet("", flag.ExitOnError)
+	fl.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), `
+Create a multisig transaction. By default a contract creation transaction is
+created. This can be changed into a contract update if the contract ID is
+provided.
+
+Created message does not contain any participants details. Attaching
+participants must be done by another command.
+		`)
+		fl.PrintDefaults()
+	}
+	var (
+		updateFl              = flHex(fl, "update", "", "If a multisig contract ID is provided, a multisig contract update instead of creation message is created.")
+		activationThresholdFl = fl.Uint("activation", 0, "Activation threshold value. Must be greater than 0.")
+		adminThresholdFl      = fl.Uint("admin", 0, "Admin threshold value. Must be greater than 0.")
+	)
+	fl.Parse(args)
+
+	if *activationThresholdFl == 0 {
+		flagDie("activation threshold cannot be zero")
+	}
+	if *adminThresholdFl == 0 {
+		flagDie("admin threshold cannot be zero")
+	}
+
+	var tx app.Tx
+
+	if len(*updateFl) != 0 {
+		tx = app.Tx{
+			Sum: &app.Tx_UpdateContractMsg{
+				UpdateContractMsg: &multisig.UpdateContractMsg{
+					Metadata:            &weave.Metadata{Schema: 1},
+					ContractID:          *updateFl,
+					ActivationThreshold: multisig.Weight(*activationThresholdFl),
+					AdminThreshold:      multisig.Weight(*adminThresholdFl),
+				},
+			},
+		}
+	} else {
+		tx = app.Tx{
+			Sum: &app.Tx_CreateContractMsg{
+				CreateContractMsg: &multisig.CreateContractMsg{
+					Metadata:            &weave.Metadata{Schema: 1},
+					ActivationThreshold: multisig.Weight(*activationThresholdFl),
+					AdminThreshold:      multisig.Weight(*adminThresholdFl),
+				},
+			},
+		}
+	}
+
+	_, err := writeTx(output, &tx)
+	return err
+}
+
+func cmdWithMultisigParticipant(input io.Reader, output io.Writer, args []string) error {
+	fl := flag.NewFlagSet("", flag.ExitOnError)
+	fl.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), `
+Read a multisig create or update transaction from the input and modify it by
+adding a participant. Returned transaction is the original content with a
+participant added.
+		`)
+		fl.PrintDefaults()
+	}
+	var (
+		sigFl    = flAddress(fl, "sig", "", "Participant signature/address.")
+		weightFl = fl.Uint("weight", 1, "Participant weight.")
+	)
+	fl.Parse(args)
+
+	tx, _, err := readTx(input)
+	if err != nil {
+		return fmt.Errorf("cannot read input transaction: %s", err)
+	}
+
+	msg, err := tx.GetMsg()
+	if err != nil {
+		return fmt.Errorf("cannot extract transaction message: %s", err)
+	}
+
+	switch msg := msg.(type) {
+	case *multisig.CreateContractMsg:
+		msg.Participants = append(msg.Participants, &multisig.Participant{
+			Signature: *sigFl,
+			Weight:    multisig.Weight(*weightFl),
+		})
+	case *multisig.UpdateContractMsg:
+		msg.Participants = append(msg.Participants, &multisig.Participant{
+			Signature: *sigFl,
+			Weight:    multisig.Weight(*weightFl),
+		})
+	default:
+		return fmt.Errorf("message %T cannot be modified to contain multisig participant", msg)
+	}
+
+	_, err = writeTx(output, tx)
+	return nil
+}
+
+func cmdWithMultisig(input io.Reader, output io.Writer, args []string) error {
+	fl := flag.NewFlagSet("", flag.ExitOnError)
+	fl.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), `
+Read a transaction from the input and attach any number of provided multisig
+contract IDs to that transaction.
+		`)
+		fl.PrintDefaults()
+	}
+	fl.Parse(args)
+
+	tx, _, err := readTx(input)
+	if err != nil {
+		return fmt.Errorf("cannot read input transaction: %s", err)
+	}
+
+	for _, mid := range fl.Args() {
+		tx.Multisig = append(tx.Multisig, []byte(mid))
+	}
+
+	_, err = writeTx(output, tx)
+	return err
+}
