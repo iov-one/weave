8608304f71510c52add0f252e075fb9488fcd27d
Piotr Husiatyński <phusiatynski@gmail.com>
2019-06-17 08:38:05 +0200

    x/username: update doc.go

--- x/username/doc.go
+++ x/username/doc.go
@@ -4,10 +4,8 @@ address.
 
 Each username consist of a name and a domain. Username format is
 `<name>*<namespace>`, for example `alice*iov`. Each username is unique. Any
-number of usernames can point to the same blockchain address.
-
-A blockchain address is a blockchain ID and a address value that is specific to
-that network.
+number of usernames can point to the same location. A location is a blockchain
+ID and an address value that is specific to that network.
 
 You can think of the functionality provided by this package similar to what
 domain name server does. This functionality is narrowed to blockchains only.
