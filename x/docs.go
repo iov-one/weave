11aae2681bcc13763e3c0669061f8f3f69e5216c
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-24 15:13:47 +0200

    Add documentation on message naming

--- x/docs.go
+++ x/docs.go
@@ -10,5 +10,9 @@ All of them provide functionality commonly needed by blockchains.
 You are welcome to import them if desired, but if they
 don't match your particular needs, you may also write your
 own extensions and use them instead.
+
+Note that protobuf types in exported code will be prefixed by
+the package, so follow standard go naming conventions and avoid
+stutter. Use eg. `escrow.CreateMsg` in place of `escrow.CreateEscrowMsg`.
 */
 package x
