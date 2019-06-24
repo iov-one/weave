4ce8afdc0cb084a9d0813b6f6c4de6eaf06c2ede
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-24 16:09:57 +0200

    Removed examples directory

--- examples/mycoind/commands/docs.go
+++
@@ -1,8 +0,0 @@
-/*
-Package commands has integration tests of the mycoind
-application.
-
-Tests the application plays together properly with the
-cli commands.
-*/
-package commands
--- examples/mycoind/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker rm -f iov-tm || true
-docker run --name iov-tm --rm -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 init
-docker run --name iov-tm -d -p26657:26657 -v ~/.mycoind:/tendermint iov1/tendermint:0.21.0 node --proxy_app="tcp://host.docker.internal:46658"
-
-mycoind init
-mycoind start
