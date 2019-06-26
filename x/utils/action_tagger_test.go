ab5789faad59a9b39ace008d821492fd00076754
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-26 15:28:10 +0200

    Add more complete tests, error in batch tagging

--- x/utils/action_tagger_test.go
+++ x/utils/action_tagger_test.go
@@ -1,30 +1,77 @@
-package utils
+package utils_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/iov-one/weave"
+	"github.com/iov-one/weave/app"
 	"github.com/iov-one/weave/errors"
 	"github.com/iov-one/weave/store"
 	"github.com/iov-one/weave/weavetest"
 	"github.com/iov-one/weave/weavetest/assert"
+	"github.com/iov-one/weave/x/batch"
+	"github.com/iov-one/weave/x/utils"
 	"github.com/tendermint/tendermint/libs/common"
 )
 
+func stringTag(key, value string) common.KVPair {
+	return common.KVPair{
+		Key:   []byte(key),
+		Value: []byte(value),
+	}
+}
+
 func TestActionTagger(t *testing.T) {
 	cases := map[string]struct {
-		wrap weave.Decorator
-		h    weave.Handler
-		tx   weave.Tx
-		err  *errors.Error
-		tags []common.KVPair
+		stack weave.Handler
+		tx    weave.Tx
+		err   *errors.Error
+		tags  []common.KVPair
 	}{
 		"simple call": {
-			wrap: NewActionTagger(),
-			h:    &weavetest.Handler{},
+			stack: app.ChainDecorators(utils.NewActionTagger()).WithHandler(
+				&weavetest.Handler{},
+			),
 			tx:   &weavetest.Tx{Msg: &weavetest.Msg{RoutePath: "foobar/create"}},
-			tags: []common.KVPair{{Key: []byte("action"), Value: []byte("foobar/create")}},
+			tags: []common.KVPair{stringTag(utils.ActionKey, "foobar/create")},
+		},
+		"passes through error": {
+			stack: app.ChainDecorators(utils.NewActionTagger()).WithHandler(
+				&weavetest.Handler{DeliverErr: errors.ErrHuman},
+			),
+			tx:  &weavetest.Tx{Msg: &weavetest.Msg{RoutePath: "foobar/create"}},
+			err: errors.ErrHuman,
+		},
+		"tags are additive": {
+			stack: app.ChainDecorators(utils.NewActionTagger()).WithHandler(
+				&weavetest.Handler{
+					DeliverResult: weave.DeliverResult{Tags: []common.KVPair{stringTag(utils.ActionKey, "random")}},
+				},
+			),
+			tx:   &weavetest.Tx{Msg: &weavetest.Msg{RoutePath: "foobar/create"}},
+			tags: []common.KVPair{stringTag(utils.ActionKey, "random"), stringTag(utils.ActionKey, "foobar/create")},
+		},
+		"all in batch are tagged": {
+			stack: app.ChainDecorators(
+				batch.NewDecorator(),
+				utils.NewActionTagger(),
+			).WithHandler(
+				&weavetest.Handler{},
+			),
+			tx: &weavetest.Tx{Msg: &batchMsg{
+				msgs: []weave.Msg{
+					&weavetest.Msg{RoutePath: "username/register"},
+					&weavetest.Msg{RoutePath: "cash/send"},
+					&weavetest.Msg{RoutePath: "gov/vote"},
+				},
+			}},
+			tags: []common.KVPair{
+				stringTag(utils.ActionKey, "username/register"),
+				stringTag(utils.ActionKey, "cash/send"),
+				stringTag(utils.ActionKey, "gov/vote"),
+			},
 		},
 	}
 
@@ -34,7 +81,7 @@ func TestActionTagger(t *testing.T) {
 			store := store.MemStore()
 
 			// we get tagged on success
-			res, err := tc.wrap.Deliver(ctx, store, tc.tx, tc.h)
+			res, err := tc.stack.Deliver(ctx, store, tc.tx)
 			if tc.err != nil {
 				if !tc.err.Is(err) {
 					t.Fatalf("Unexpected error type returned: %v", err)
@@ -42,6 +89,10 @@ func TestActionTagger(t *testing.T) {
 				return
 			}
 			assert.Nil(t, err)
+			fmt.Println("")
+			for _, t := range res.Tags {
+				fmt.Printf("%s = %s\n", string(t.Key), string(t.Value))
+			}
 			assert.Equal(t, len(tc.tags), len(res.Tags))
 			for i := range tc.tags {
 				assert.Equal(t, string(tc.tags[i].Key), string(res.Tags[i].Key))
@@ -49,4 +100,30 @@ func TestActionTagger(t *testing.T) {
 			}
 		})
 	}
+}
+
+var _ batch.Msg = (*batchMsg)(nil)
+
+type batchMsg struct {
+	msgs []weave.Msg
+}
+
+func (m *batchMsg) Marshal() ([]byte, error) {
+	panic("implement me")
+}
+
+func (m *batchMsg) Unmarshal([]byte) error {
+	panic("implement me")
+}
+
+func (m *batchMsg) Path() string {
+	panic("implement me")
+}
+
+func (m *batchMsg) Validate() error {
+	return nil
+}
+
+func (m *batchMsg) MsgList() ([]weave.Msg, error) {
+	return m.msgs, nil
 }
