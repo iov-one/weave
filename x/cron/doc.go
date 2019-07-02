862ae9147eaa282b10d716ef4b446a644c6cb0fe
Piotr Husiatyński <phusiatynski@gmail.com>
2019-07-02 11:28:53 +0200

    move cron package to x/

---
+++ x/cron/doc.go
@@ -0,0 +1,8 @@
+/*
+Package cron implements delayed message execution.
+
+This package provides a queue implementation for scheduling message for
+execution in the future and weave.Ticker compatible task runner.
+
+*/
+package cron
