a87fbcfd568401b2c897dfb3c4b9658803f09df2
Roman Useinov <roman.useinov@gmail.com>
2019-07-11 10:06:46 +0200

    weavetest: introduced IsErr

--- weavetest/assert/assert.go
+++ weavetest/assert/assert.go
@@ -105,3 +105,22 @@ func FieldError(t testing.TB, err error, fieldName string, want *errors.Error) {
 
 	}
 }
+
+
+// IsErr is a convenient helper that checks if the errors are a match
+// and prints out the difference if not as well as failing the assertion.
+func IsErr(t testing.TB, want, got error) {
+	t.Helper()
+
+	if want == got {
+		return
+	}
+
+	if want, ok := want.(interface {
+		Is(error) bool
+	}); ok && want.Is(got) {
+		return
+	}
+
+	t.Fatalf("want %q, got %+v", want, got)
+}
