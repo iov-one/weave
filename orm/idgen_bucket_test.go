0deb4d076820c47137e2f97f64f048c61b0e6859
Orkun Külçe <kulceorkun@gmail.com>
2019-08-19 17:25:52 +0300

    orm: add ValidateSequence

--- orm/idgen_bucket_test.go
+++ orm/idgen_bucket_test.go
@@ -64,3 +64,35 @@ func TestIDGenBucket(t *testing.T) {
 	}
 
 }
+
+func TestValidateSequence(t *testing.T) {
+	cases := map[string]struct {
+		bytes   []byte
+		wantErr *errors.Error
+	}{
+		"success": {
+			bytes:   []byte{0, 1, 2, 3, 4, 5, 6, 7},
+			wantErr: nil,
+		},
+		"failure missing": {
+			bytes:   nil,
+			wantErr: errors.ErrEmpty,
+		},
+		"failure invalid length": {
+			bytes:   []byte{0, 1},
+			wantErr: errors.ErrInput,
+		},
+	}
+
+	for testName, tc := range cases {
+		t.Run(testName, func(t *testing.T) {
+			err := ValidateSequence(tc.bytes)
+			if !tc.wantErr.Is(err) {
+				t.Fatalf("unexpected error: %+v", err)
+			}
+			if tc.wantErr != nil {
+				return
+			}
+		})
+	}
+}
