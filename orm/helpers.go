71ade57e264301e50c3363dca4835b32b85c9daa
Orkun Külçe <kulceorkun@gmail.com>
2019-11-06 15:54:08 +0300

    implement WithLimit and ToSlice helper methods

---
+++ orm/helpers.go
@@ -0,0 +1,70 @@
+package orm
+
+import (
+	"reflect"
+
+	"github.com/iov-one/weave/errors"
+)
+
+type limitedIterator struct {
+	// n is count of remaining elements
+	n int
+	// iterator is the underlying iterator
+	iter SerialModelIterator
+}
+
+var _ SerialModelIterator = (*limitedIterator)(nil)
+
+func (l *limitedIterator) LoadNext(dest SerialModel) error {
+	if l.n > 0 {
+		l.n--
+		return l.iter.LoadNext(dest)
+	}
+	return errors.Wrap(errors.ErrIteratorDone, "iterator limit reached")
+}
+
+func (l *limitedIterator) Release() {
+	l.iter.Release()
+}
+
+func WithLimit(iter SerialModelIterator, limit int) (SerialModelIterator, error) {
+	if limit < 1 {
+		return nil, errors.Wrap(errors.ErrInput, "invalid limit")
+	}
+	return &limitedIterator{iter: iter, n: limit}, nil
+}
+
+func ToSlice(iter SerialModelIterator, destination SerialModelSlicePtr) error {
+	dest := reflect.ValueOf(destination)
+	if dest.Kind() != reflect.Ptr {
+		return errors.Wrap(errors.ErrType, "destination must be a pointer to slice of SerialModels")
+	}
+	if dest.IsNil() {
+		return errors.Wrap(errors.ErrImmutable, "got nil pointer")
+	}
+	dest = dest.Elem()
+	if dest.Kind() != reflect.Slice {
+		return errors.Wrap(errors.ErrType, "destination must be a pointer to slice of SerialModels")
+	}
+
+	// Allocate memory depending on the type of the destination
+	var elemTyp reflect.Type
+	if dest.Type().Elem().Kind() != reflect.Ptr {
+		elemTyp = dest.Type().Elem()
+	} else {
+		elemTyp = dest.Type().Elem().Elem()
+	}
+
+	// Consume all the elements in iter to the destination
+	for {
+		d := reflect.New(elemTyp).Interface().(SerialModel)
+		if err := iter.LoadNext(d); err != nil {
+			if !errors.ErrIteratorDone.Is(err) {
+				return err
+			}
+			// if iterator dones error received, means successfuly loaded iterator
+			return nil
+		}
+		dest.Set(reflect.Append(dest, reflect.ValueOf(d)))
+	}
+}
---
+++ orm/helpers_test.go
@@ -0,0 +1,111 @@
+package orm
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/iov-one/weave/errors"
+	"github.com/iov-one/weave/store"
+	"github.com/iov-one/weave/weavetest/assert"
+)
+
+func TestWithLimit(t *testing.T) {
+	db := store.MemStore()
+
+	b := NewSerialModelBucket("cnts", &CounterWithID{},
+		WithIndexSerial("counter", func(Object) ([]byte, error) { return []byte("all"), nil }, false))
+
+	var expected []*CounterWithID
+	for i := 0; i < 30; i++ {
+		c := &CounterWithID{
+			Count: int64(i * 1000),
+		}
+		expected = append(expected, c)
+	}
+
+	for _, e := range expected {
+		// make sure we point to value in array, so this PrimaryKey gets set
+		err := b.Create(db, e)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+	}
+
+	iter, err := b.IndexScan(db, "counter", []byte("all"), false)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	// should return error when received limit lesser than 1
+	limit := 0
+	limitedIter, err := WithLimit(iter, limit)
+	if err != nil && !errors.ErrInput.Is(err) {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	limit = -100
+	limitedIter, err = WithLimit(iter, limit)
+	if err != nil && !errors.ErrInput.Is(err) {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// limit 1 should work
+	limit = 1
+	limitedIter, err = WithLimit(iter, limit)
+	if err != nil && !errors.ErrInput.Is(err) {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	limit = 10
+	limitedIter, err = WithLimit(iter, limit)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	var dest CounterWithID
+	for i := 0; i < 10; i++ {
+		if err := limitedIter.LoadNext(&dest); err != nil {
+			if !errors.ErrIteratorDone.Is(err) {
+				t.Fatalf("unexpected error: %s", err)
+			}
+		}
+		if !reflect.DeepEqual(expected[i], &dest) {
+			t.Errorf("values do not match, expected: %+v, got: %+v", expected[i], &dest)
+		}
+	}
+}
+
+func TestToSlice(t *testing.T) {
+	db := store.MemStore()
+
+	b := NewSerialModelBucket("cnts", &CounterWithID{},
+		WithIndexSerial("counter", func(Object) ([]byte, error) { return []byte("all"), nil }, false))
+
+	var expected []*CounterWithID
+	for i := 0; i > 30; i++ {
+		c := &CounterWithID{
+			Count: int64(i * 1000),
+		}
+		expected = append(expected, c)
+	}
+	for _, e := range expected {
+		// make sure we point to value in array, so this PrimaryKey gets set
+		err := b.Create(db, e)
+		assert.Nil(t, err)
+	}
+
+	iter, err := b.IndexScan(db, "counter", []byte("all"), false)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	var dest []*CounterWithID
+	err = ToSlice(iter, &dest)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if !reflect.DeepEqual(dest, expected) {
+		t.Errorf("values do not match, expected: %+v, got: %+v", expected, dest)
+	}
+}
