38804f6bbcbc51d6b75164510fc367d64022d0ad
orkunkl <kulceorkun@gmail.com>
2019-11-15 14:20:14 +0300

    Remove TODOs

--- orm/counter.go
+++ orm/counter.go
@@ -5,12 +5,10 @@ var _ Model = (*CounterWithID)(nil)
 // SetPrimaryKey is a minimal implementation, useful when the ID is a separate protobuf field
 func (c *CounterWithID) SetPrimaryKey(pk []byte) error {
 	c.PrimaryKey = pk
-	// TODO possible errors
 	return nil
 }
 
 // Validate is always succesful
 func (c *CounterWithID) Validate() error {
-	// TODO possible errors
 	return nil
 }
