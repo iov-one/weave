566cc88a30e93509c5d6e4af676a45b1e20c0082
orkunkl <kulceorkun@gmail.com>
2019-11-15 14:27:01 +0300

    Exclude error on return value of LimitIterator

--- orm/helpers.go
+++ orm/helpers.go
@@ -29,11 +29,8 @@ func (l *limitedIterator) Release() {
 	l.iter.Release()
 }
 
-func LimitIterator(iter SerialModelIterator, limit int) (SerialModelIterator, error) {
-	if limit < 1 {
-		return nil, errors.Wrap(errors.ErrInput, "invalid limit")
-	}
-	return &limitedIterator{iter: iter, remaining: limit}, nil
+func LimitIterator(iter SerialModelIterator, limit int) SerialModelIterator {
+	return &limitedIterator{iter: iter, remaining: limit}
 }
 
 func ToSlice(iter SerialModelIterator, destination SerialModelSlicePtr) error {
